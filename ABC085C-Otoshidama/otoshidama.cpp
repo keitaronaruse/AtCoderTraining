ebc15a8bab57ca6acdecd8c1744994ec8c2a7038
keitaronaruse <naruse@u-aizu.ac.jp>
2021-11-17 22:41:59 +0900

    updated

---
+++ ABC085C-Otoshidama/otoshidama.cpp
@@ -0,0 +1,27 @@
+/*
+    ABC085C - Otoshidama
+        https://atcoder.jp/contests/abs/tasks/abc085_c
+        Author: Keitaro Naruse
+        Date: 2021-11-17
+        MIT License
+*/
+
+#include <iostream>
+
+int main()
+{
+    //  Read N
+    size_t N( 0 );
+    std::cin >> N;
+    std::cerr << N << std::endl;
+    
+    //  Read Y
+    size_t Y( 0 );
+    std::cin >> Y;
+    std::cerr << Y << std::endl;
+
+    //  Finalize
+    std::cerr << "Normally terminated." << std::endl;
+
+    return( 0 );
+}
