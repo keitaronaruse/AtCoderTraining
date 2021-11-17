44e71a57b3c4828167808ae8cecea4c8908710f3
keitaronaruse <naruse@u-aizu.ac.jp>
2021-11-17 12:43:31 +0900

    updated

--- ABC087B-Coins/coins.cpp
+++ ABC087B-Coins/coins.cpp
@@ -1,7 +1,7 @@
 /*
     ABC087B Coins
         Author: Keitaro Naruse
-        Date:   2021-11-16
+        Date:   2021-11-17
         MIT License
 */
 
@@ -18,31 +18,52 @@ int main()
     //  C: the number of  50 coins
     //  X: a target price
     std::cin >> A >> B >> C >> X;
-    std::cout << A << " " << B << " " << C << " " << X << std::endl;
+    // std::cout << A << " " << B << " " << C << " " << X << std::endl;
 
     //  Look-up table of prices by the number of 500 coins
     std::vector< size_t > price_table_500_coins( A+1 );
-    for(size_t i = 0; i != A+1; i++) {
+    for( size_t i = 0; i != A+1; ++i ) {
         price_table_500_coins.at( i ) = 500 * i;
-        std::cerr << price_table_500_coins.at( i ) << " ";
+        // std::cerr << price_table_500_coins.at( i ) << " ";
     }
-    std::cerr << std::endl;
+    // std::cerr << std::endl;
+
     //  Look-up table of prices by the number of 100 coins
     std::vector< size_t > price_table_100_coins( B+1 );
-    for(size_t i = 0; i != B+1; i++) {
-        price_table_100_coins.at( i ) = 100 * i;
-        std::cerr << price_table_100_coins.at( i ) << " ";
+    for( size_t j = 0; j != B+1; ++j ) {
+        price_table_100_coins.at( j ) = 100 * j;
+        // std::cerr << price_table_100_coins.at( j ) << " ";
     }
-    std::cerr << std::endl;
+    // std::cerr << std::endl;
+
     //  Look-up table of prices by the number of 50 coins
     std::vector< size_t > price_table_50_coins( C+1 );
-    for(size_t i = 0; i != C+1; i++) {
-        price_table_50_coins.at( i ) = 50 * i;
-        std::cerr << price_table_50_coins.at( i ) << " ";
+    for( size_t k = 0; k != C+1; ++k ) {
+        price_table_50_coins.at( k ) = 50 * k;
+        // std::cerr << price_table_50_coins.at( k ) << " ";
     }
-    std::cerr << std::endl;
+    // std::cerr << std::endl;
+
+    //  Couting up
+    int combinations( 0 );
+    for( size_t i = 0; i != A+1; ++i ) {
+        if( X >= price_table_500_coins.at( i ) ) {
+            for( size_t j = 0; j != B+1; ++j ) {
+                if( X >= price_table_500_coins.at( i ) + price_table_100_coins.at( j ) ) {
+                    for( size_t k = 0; k != C+1; ++k ) {
+                        if( X == price_table_500_coins.at( i ) + price_table_100_coins.at( j ) + price_table_50_coins.at( k ) ) {
+                            ++combinations;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //  Results
+    std::cout << combinations << std::endl;
 
     //  Finalize
-    std::cerr << "Normally terminated." << std::endl;
+    // std::cerr << "Normally terminated." << std::endl;
     return( 0 );
 }
