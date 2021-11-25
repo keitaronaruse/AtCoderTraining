ae80d78ebb40e502f445dc1d609e134c79dd9881
keitaronaruse <naruse@u-aizu.ac.jp>
2021-11-25 16:14:16 +0900

    updated

--- ABC086C-Traveling/traveling.cpp
+++ ABC086C-Traveling/traveling.cpp
@@ -2,12 +2,50 @@
     ABC086C - Traveling
         https://atcoder.jp/contests/abs/tasks/arc089_a
         Author: Keitaro Naruse
-        Date:   2021-11-23
+        Date:   2021-11-23, 2021-11-25
         MIT License
 */
 
+//  Solution
+//  Manhattan distance: D = L1(xj, yj, yi, yi)
+//  if tj-ti = D + 2k, k = 0, 1, ... -> Yes
+
 #include <iostream>
 #include <vector>
+
+/*
+    L1()
+        returns L1 norm
+*/
+int L1(int xj, int yj, int xi, int yi)
+{
+    int dx = ( xj > xi )? xj - xi : xi - xj; 
+    int dy = ( yj > yi )? yj - yi : yi - yj;
+
+    return(dx + dy); 
+}
+
+bool is_possible_move( int dT, int dL )
+{
+    int k( 0 );
+    int h = dT - dL - k; 
+    
+    while( true ) {
+        if( h == 0 ) {
+            return( true );
+        }
+        else if( h > 0 ) {
+            k += 2;
+            h = dT - dL - k; 
+        }
+        else {
+            return( false );
+        }
+    }
+
+    return( false );
+}
+
 int main()
 {
     //  Read N
@@ -17,13 +55,29 @@ int main()
     std::cerr << N << std::endl;
 
     //  Read (ti, xi, ni)
-    std::vector< int > t( N, 0 );
-    std::vector< int > x( N, 0 );
-    std::vector< int > y( N, 0 );
+    std::vector< int > t( N + 1, 0 );
+    std::vector< int > x( N + 1, 0 );
+    std::vector< int > y( N + 1, 0 );
     for( size_t i = 0; i != N; i++ ) {
-        std::cin >> t.at( i ) >> x.at( i ) >> y.at( i );
-        //  Debug
-        std::cerr << t.at( i ) << " " << x.at( i ) << " " << y.at( i )<< std::endl;
+        std::cin >> t.at( i + 1 ) >> x.at( i + 1 ) >> y.at( i + 1);
+    }
+    //  Debug
+    for( size_t i = 0; i != N + 1; i++ ) {
+        std::cerr << t.at( i ) << " " << x.at( i ) << " " << y.at( i ) << std::endl;
+    }
+
+    //  Main
+    for( size_t i = 0; i != N; i++ ) {
+        int dT = t.at( i + 1 ) - t.at( i );
+        int dL = L1( x.at( i + 1 ), y.at( i + 1 ), x.at( i ), y.at( i ) );
+        std::cerr << dT << " " <<  dL << std::endl;
+        
+        if( is_possible_move( dT, dL ) ) {
+            std::cout << "YES" << std::endl;
+        }
+        else {
+            std::cout << "NO" << std::endl;
+        }
     }
 
     //  Finalize
