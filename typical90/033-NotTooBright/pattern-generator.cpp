f0cee1297682f2c47905571a0cb2c359cfc0f894
keitaronaruse <naruse@u-aizu.ac.jp>
2021-11-28 15:29:08 +0900

    updated

--- typical90/033-NotTooBright/pattern-generator.cpp
+++ typical90/033-NotTooBright/pattern-generator.cpp
@@ -14,14 +14,31 @@
 int main()
 {
     //  Initialize
-    const size_t H = 5, W = 5;
+    const size_t H = 2, W = 2;
 
     //  Main
     //  Make and disply all possible patterns
-    for( int i = 0; i != ( 1 << H + W ); i ++ ) {
-        std::bitset< ( H + W ) > b( i );
-        std::cout << b << std::endl;
+    int num_good = 0;
+    for( int k = 0; k != ( 1 << H + W ); k ++ ) {
+        std::bitset< ( H + W ) > b( k );
+        if( ( b.count() == 0 ) || ( b.count() == 1 ) ) {
+            num_good ++;
+            //  Debug
+            std::cerr << b << std::endl;
+            //  Debug
+            //  b[0] b[1]
+            //  b[2] b[3]
+            // ( b[0] == true ) ? std::cerr << "#" : std::cerr << "."; 
+            // ( b[1] == true ) ? std::cerr << "#" : std::cerr << ".";
+            // std::cerr << std::endl; 
+            // ( b[2] == true ) ? std::cerr << "#" : std::cerr << "."; 
+            // ( b[3] == true ) ? std::cerr << "#" : std::cerr << ".";
+            // std::cerr << std::endl; 
+        }
     }
+
+    //  Display result
+    std::cout << num_good << std::endl;
 
     //  Finalize
     //  Debug
