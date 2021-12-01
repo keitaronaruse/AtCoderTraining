32247b4581017ed44466bb3e4425826056d6b469
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-01 19:29:38 +0900

    updated

--- typical90/061-Deck/deck.cpp
+++ typical90/061-Deck/deck.cpp
@@ -2,7 +2,7 @@
     061 - Deck（★2）
         https://atcoder.jp/contests/typical90/tasks/typical90_bi
         Author: Keitaro Naruse
-        Date:   2021-11-30
+        Date:   2021-11-30, 2021-12-01
         MIT License
 */
 
@@ -18,20 +18,34 @@ int main()
     int Q = 0;
     std::cin >> Q;
     //  Debug
-    std::cerr << Q << std::endl;
+    // std::cerr << Q << std::endl;
     
-    //  Read ti and make xi
-    std::vector< int > t( Q ), x( Q );
+    //  Main Read ti and make xi
+    //  Deck as duque
+    std::deque<int> deck;
     for( int i = 0; i != Q; i ++ ) {
-        std::cin >> t.at( i ) >> x.at( i );
+        int t = 0, x = 0;
+        std::cin >> t >> x;
         //  Debug
-        std::cerr << t.at( i ) << " " << x.at( i ) << std::endl;
+        // std::cerr << t << " " << x << std::endl;
+
+        if( t == 1 ) {
+            deck.push_front( x );
+        }
+        else if( t == 2 ) {
+            deck.push_back( x );
+        }
+        else if( t == 3 ) {
+            std::cout << deck.at( x - 1 ) << std::endl;
+        }
+        else {
+            //  Debug
+            std::cerr << "Never happens." << std::endl;
+        }
     }
 
-    //  Main
-    std::deque<int> deck;
-
     //  Finalize
-    std::cerr << "Normally terminated." << std::endl;
+    //  Debug
+    // std::cerr << "Normally terminated." << std::endl;
     return( 0 );
 }
