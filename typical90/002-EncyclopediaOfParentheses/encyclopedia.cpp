310c6f4b63b22edc2d22ca585476e40f5dcffa94
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-02 23:54:24 +0900

    updated

--- typical90/002-EncyclopediaOfParentheses/encyclopedia.cpp
+++ typical90/002-EncyclopediaOfParentheses/encyclopedia.cpp
@@ -10,7 +10,8 @@
 
 #include <iostream>
 #include <string>
-#include <list>
+#include <vector>
+#include <set>
 
 int main()
 {
@@ -21,16 +22,41 @@ int main()
     std::cerr << N << std::endl;
 
     //  Main
-    if( N % 2 == 0 ) {
-        //  valide case
-        std::list< std::string > words;
-        std::string word;
-        if( N == 2 ) {
-            word = "()";
-            words.push_back( word );
-        }
+    //  Make right words
+    std::vector< std::set< std::string > > words( N/2 );
+
+    //  Axiom: N == 2, i == 0
+    words.at( 0 ).insert( "()" );
+    for( int i = 1; i != N / 2; i++ ) {
+        //  N == 4, i == 1, S4
+        //  (S2)
+        //  (j = 0; j != i == 1; j++) 
+        //  j == 0(S2), k != i-j-1 == 1-0-1 == 0(S2) 
+        //  S2S2
+        
+        //  N == 6, i == 2, S6
+        //  (S4)
+        //  (j = 0; j != i == 2; j++) 
+        //  j == 0(S2), k != i-j-1 == 2-0-1 == 1(S4) 
+        //  S2S4
+        //  j == 1(S4), k != i-j-1 == 2-1-1 == 0(S2) 
+        //  S4S2
+
+        //  N == 8, i == 3
+        //  (S6)
+        //  (j = 0; j != i == 3; j++) 
+        //  j == 0(S2), k != i-j-1 == 3-0-1 == 2(S6) 
+        //  S2S6
+        //  j == 1(S4), k != i-j-1 == 3-1-1 == 1(S4) 
+        //  S4S4
+        //  j == 2(S4), k != i-j-1 == 3-2-1 == 0(S2) 
+        //  S6S2
+    }
+
+    //  Display result
+    if( N % 2 == 0) {
         //  Display output
-        for( const auto& e : words ) {
+        for( const auto& e : words.at( N / 2 - 1) ) {
             std::cout << e << std::endl;
         }
     }
@@ -39,7 +65,6 @@ int main()
         //  Display nothing
         std::cout << std::endl;
     }
-
 
     //  Finalize
     //  Debug
