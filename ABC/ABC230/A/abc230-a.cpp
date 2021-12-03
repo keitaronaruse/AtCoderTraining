81cf7446da168e9b1941b250ce52d5af538c2bd0
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-03 15:53:48 +0900

    updated

---
+++ ABC/ABC230/A/abc230-a.cpp
@@ -0,0 +1,16 @@
+/*
+    ABC230 Problem A
+        https://atcoder.jp/contests/abc230
+        Author: Keitaro Naruse
+        Date:   2021-12-03
+        MIT License
+*/
+
+#include <iostream>
+
+int main()
+{
+    //  Debug
+    std::cerr << "Normally terminated." << std::endl;
+    return( 0 );
+}
