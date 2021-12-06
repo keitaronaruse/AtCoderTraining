54fb4a12b8db2b25ae90318ef1d4b0eaaeb41de6
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-06 17:44:44 +0900

    updated

--- ABC/ABC231/C/abc231-c.cpp
+++ ABC/ABC231/C/abc231-c.cpp
@@ -6,13 +6,23 @@
 */
 
 #include <iostream>
+#include <vector>
 
 int main()
 {
     //  Read N
     int N = 0;
     std::cin >> N;
-
+    //  Read Ai
+    std::vector< int > A( N );
+    for( int i = 0; i < N; i++ ) {
+        std::cin >> A.at( i );
+        //  Debug
+        std::cout << A.at( i ) << " ";
+    }
+    //  Debug
+    std::cout << std::endl;
+    
     //  Debug
     std::cerr << "Normally terminated." << std::endl;
     return( 0 );
---
+++ typical90/016-MinimumCoins/minimum-coins.cpp
@@ -0,0 +1,65 @@
+/*
+    016 - Minimum Coins（★3）
+        https://atcoder.jp/contests/typical90/tasks/typical90_p
+        Author: Keitaro Naruse
+        Date:   2021-12-06
+        MIT License
+*/
+
+// # Solution
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    //  Read N
+    int N = 0;
+    std::cin >> N;
+    //  Debug
+    // std::cerr << N << std::endl;
+
+    //  Read Ai
+    std::vector< int > A( N );
+    for( int i = 0; i < N; i ++ ) {
+        std::cin >> A.at( i );
+        //  Debug
+        // std::cerr << A.at( i ) << " ";
+    }
+    // std::cerr << std::endl;
+
+    //  Read Bi
+    std::vector< int > B( N );
+    for( int i = 0; i < N; i ++ ) {
+        std::cin >> B.at( i );
+        //  Debug
+        // std::cerr << B.at( i ) << " ";
+    }
+    // std::cerr << std::endl;
+
+    //  Sort A and B
+    std::sort( A.begin(), A.end() );
+    std::sort( B.begin(), B.end() );
+    long long inconvenience = 0LL;
+    for( int i = 0; i < N; i ++ ) {
+        inconvenience += ( long long )( A.at( i ) > B.at( i ) ) ? A.at( i ) - B.at( i ) : B.at( i ) - A.at( i );
+    }
+    std::cout << inconvenience << std::endl;
+
+    // Debug
+    // for( const auto& a : A ) {
+    //     std::cerr << a << " ";
+    // }
+    // std::cerr << std::endl;
+
+    // for( const auto& b : B ) {
+    //     std::cerr << b << " ";
+    // }
+    // std::cerr << std::endl;    
+
+    //  Finalize
+    //  Debug
+    // std::cerr << "Normally terminated." << std::endl;
+    return( 0 );
+}
