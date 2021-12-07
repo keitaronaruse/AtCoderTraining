751ffccdcbf4d60f3ca4ff2b3740656fe87a87b7
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-07 18:48:03 +0900

    updated

--- typical90/016-MinimumCoins/minimum-coins.cpp
+++ typical90/016-MinimumCoins/minimum-coins.cpp
@@ -7,40 +7,46 @@
 */
 
 // # Solution
-// - Loop for k, the number of coins
-// - Split k into (ka, kb, kc) and judge if ka*A + kb*B + kc*C == N
+// - Loop i for the number of A coins
+// - Loop j for the number of B coins
+// - the number of C coins is determined by N = i*A + j*B + k*C
 
 #include <iostream>
 #include <vector>
-#include <cstdio>
+#include <algorithm>
 
 int main()
 {
     //  The number of maximum coins
-    // const int K = 9999;
-    const int K = 500;
+    const int L = 9999;
     //  Read N, A, B, C
     int N = 0, A = 0, B = 0, C = 0;
     std::cin >> N >> A >> B >> C;
     //  Debug
-    std::cerr << N << std::endl
-    << A << " " << B << " " << C << std::endl;
-
+    // std::cerr << N << std::endl << A << " " << B << " " << C << std::endl;
+    
     //  Main
-    long long count = 0LL;
-    for( int k = 0; k <= K; k ++ ) {
-        for( int ka = 0; ka <= k; ka ++ ) {
-            for( int kb = 0; kb <= k - ka; kb ++ ) {
-                for( int kc = 0; kc <= k - ka - kb; kc ++ ) {
-                    count ++;
+    int minimum_coins = L;
+    for( int i = 0; i <= L; i ++ ) {
+        for( int j = 0; j <= L - i; j ++ ) {
+            long long K = ( ( long long ) N - i * (long long)A - j * B ) / C;
+            if( K >= 0 ) {
+                int k = ( int ) K;
+                if( i * A + j * B + k * C == N ) {
+                    //  Solution
+                    minimum_coins = std::min( i + j + k, minimum_coins );
                 }
             }
         }
     }
-    std::cerr << count << std::endl;
+
+    //  Display result
+    if( minimum_coins != L ) {
+        std::cout << minimum_coins << std::endl;
+    }
 
     //  Finalize
     //  Debug
-    std::cerr << "Normally terminated." << std::endl;
+    // std::cerr << "Normally terminated." << std::endl;
     return( 0 );
 }
