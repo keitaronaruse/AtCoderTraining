db95930539de98daa8b79945e46fbb6a30114392
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-08 17:18:42 +0900

    updated

--- typical90/018-StatueOfChokudai/statue.cpp
+++ typical90/018-StatueOfChokudai/statue.cpp
@@ -28,17 +28,17 @@ int main()
     int T = 0, L = 0, X = 0, Y = 0;
     std::cin >> T >> L >> X >> Y;
     //  Debug
-    // std::cerr << T << std::endl << L << " " << X << " " << Y << std::endl;
+    std::cerr << T << std::endl << L << " " << X << " " << Y << std::endl;
     //  Read Q and Ei
     int Q = 0;
     std::cin >> Q;
     //  Debug
-    // std::cerr << Q << std::endl;
+    std::cerr << Q << std::endl;
     std::vector< int > E( Q );
     for( int i = 0; i < Q; i ++ ) {
         std::cin >> E.at( i );
         //  Debug
-        // std::cerr << E.at( i ) << std::endl;
+        std::cerr << E.at( i ) << std::endl;
     }
 
     //  Main
@@ -48,7 +48,7 @@ int main()
         double h = std::sqrt( std::pow( - X, 2.0 ) + std::pow( y - Y, 2.0 ) ); 
         double v = z; 
 
-        std::cout << std::fixed << std::setprecision(12) << atan2(v, h) / M_PI * 180.0 << std::endl;
+        std::cout << std::fixed << std::setprecision(12) << y << ","<< z << "," << atan2(v, h) / M_PI * 180.0 << std::endl;
     }
 
     //  Finalize
