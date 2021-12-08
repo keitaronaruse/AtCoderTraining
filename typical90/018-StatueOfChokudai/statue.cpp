6a508eb8d6ba6208669d38fb5208e6b14338c7cb
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-08 19:44:02 +0900

    updated

--- typical90/018-StatueOfChokudai/statue.cpp
+++ typical90/018-StatueOfChokudai/statue.cpp
@@ -28,27 +28,27 @@ int main()
     int T = 0, L = 0, X = 0, Y = 0;
     std::cin >> T >> L >> X >> Y;
     //  Debug
-    std::cerr << T << std::endl << L << " " << X << " " << Y << std::endl;
+    // std::cerr << T << std::endl << L << " " << X << " " << Y << std::endl;
     //  Read Q and Ei
     int Q = 0;
     std::cin >> Q;
     //  Debug
-    std::cerr << Q << std::endl;
+    // std::cerr << Q << std::endl;
     std::vector< int > E( Q );
     for( int i = 0; i < Q; i ++ ) {
         std::cin >> E.at( i );
         //  Debug
-        std::cerr << E.at( i ) << std::endl;
+        // std::cerr << E.at( i ) << std::endl;
     }
 
     //  Main
     for( int i = 0; i < Q; i++ ) {
-        double y = - L / 2 * std::sin( 2.0 * M_PI * E.at( i ) / T );
-        double z = - L / 2 * std::cos( 2.0 * M_PI * E.at( i ) / T ) + L / 2;
+        double y = - (double)L / 2.0 * std::sin( 2.0 * M_PI * (double)E.at( i ) / (double)T );
+        double z = - (double)L / 2.0 * std::cos( 2.0 * M_PI * (double)E.at( i ) / (double)T ) + (double)L / 2.0;
         double h = std::sqrt( std::pow( - X, 2.0 ) + std::pow( y - Y, 2.0 ) ); 
         double v = z; 
-
-        std::cout << std::fixed << std::setprecision(12) << y << ","<< z << "," << atan2(v, h) / M_PI * 180.0 << std::endl;
+        // std::cerr << E.at( i ) << ": " << y << ", " << z << ", " << h << ", " << v << ", " << std::endl; 
+        std::cout << std::fixed << std::setprecision(12) << atan2(v, h) / M_PI * 180.0 << std::endl;
     }
 
     //  Finalize
