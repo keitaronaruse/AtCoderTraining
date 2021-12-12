3e45ff92f6db1b4a8a26d2b2118023ffdbbbd086
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-12 21:07:48 +0900

    updated

--- ABC/ABC231/D/principle.cpp
+++ ABC/ABC231/D/principle.cpp
@@ -8,13 +8,46 @@
 
 #include <iostream>
 #include <vector>
+#include <set>
 #include <stack>
 #include <utility>
 
-// # Solution
-// - Make a graph of the numbers as nodes and the constraints as edges  
-// - If the degree of any nodes exceed 2, the constraints are not satisfied 
-// - If the graph includes n-loops (n > 2), the constraints are not satisfied
+int distance( int s, int g, const std::vector< std::set< int > >& adjacent )
+{
+    //  pair.first = next node
+    //  pair.second = disance
+    std::stack< std::pair< int, int > > visiting;
+
+    //  Initialize
+    if( s == g ) {
+        return( 0 );
+    }
+    for( int n : adjacent.at( s ) ) {
+        if( n == g ) {
+            return( 1 );
+        }
+        else {
+            visit.push( std::pair< int, int > ( n, 1 ) );
+        }
+    }
+
+    //  Main
+    while( !visiting.empty() ) {
+        std::pair< int, int > p = visiting.top();
+        visiting.pop();
+
+        for( int n : adjacent.at( p.first ) ) {
+            if( n == g ) {
+                return( p.second + 1 );
+            }
+            else {
+                    visiting.push( std::pair< int, int > ( n, p.second + 1) );
+            }
+        }
+    }
+    
+    return( -1 );
+}
 
 int main()
 {
@@ -28,16 +61,18 @@ int main()
     //  Degree of each of the nodes
     std::vector< int > degrees( N, 0 );
 
-    //  Read Ai and Bi and represet them as edges 
-    std::vector< std::pair< int, int > > edges( M );
+    //  Read Ai and Bi and represet them as adjacent nodes
+    std::vector< std::set< int > > adjacent( N );
     for( int j = 0; j < M; j ++ ) {
         int A = 0, B = 0;
         std::cin >> A >> B;
         // std::cerr << A << " " << B << std::endl;
         A --;
         B --;
-        edges.at( j ).first = A;
-        edges.at( j ).second = B;
+        //  Debug
+        std::cerr << A << "->" << B << "="<< distance( A, B, adjacent ) << std::endl;
+        adjacent.at( A ).insert( B );
+        adjacent.at( B ).insert( A );
         degrees.at( A ) ++ ;  
         degrees.at( B ) ++ ; 
         //  Condition 1: 
@@ -49,10 +84,15 @@ int main()
     }
 
     //  Debug
-    //  Display edges
-    for( auto e : edges ) {
-        std::cerr << e.first << " " << e.second << std::endl;
+    //  Display adjacent nodes
+    for( int i = 0; i < N; i ++ ) {
+        std::cerr << i << ": "; 
+        for( auto v : adjacent.at( i ) ) {
+            std::cerr << v << " ";
+        }
+        std::cerr << std::endl;
     }
+
     //  Display degrees
     for( auto d : degrees ) {
         std::cerr << d << " ";
