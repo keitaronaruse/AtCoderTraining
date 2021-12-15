c15485aec1aea11219506c1cf26713026281da6e
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-15 22:46:57 +0900

    updated

--- typical90/048-IWillNotDropOut/i-will-not-drop-out.cpp
+++ typical90/048-IWillNotDropOut/i-will-not-drop-out.cpp
@@ -2,7 +2,7 @@
     048 - I will not drop out（★3）
         https://atcoder.jp/contests/typical90/tasks/typical90_av
         Author: Keitaro Naruse
-        Date:   2021-12-14
+        Date:   2021-12-15
         MIT License
 */
 
@@ -16,27 +16,40 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+bool is_larger( int a, int b )
+{
+    return( a > b );
+}
 
 int main()
 {
     //  Read N and K
     int N = 0, K = 0; 
     std::cin >> N >> K;
-    std::cerr << N << " " << K << std::endl;
+    // std::cerr << N << " " << K << std::endl;
 
-    //  Read Ai and Bim and make Ci
-    std::vector< int > A( N, 0 ), B( N, 0 ), C( N, 0 );
+    //  Read Ai and Bi, and make Ci
+    std::vector< int > s( 2 * N, 0 );
     for( int i = 0; i < N; i ++ ) {
-        std::cin >> A.at( i ) >> B.at( i );
-        C.at( i ) = A.at( i ) - B.at( i );
-        std::cerr << A.at( i ) << " " << B.at( i ) << " " << C.at( i ) << std::endl;
+        int a = 0, b = 0;
+        std::cin >> a >> b;
+        s.at( 2 * i ) = b; 
+        s.at( 2 * i + 1) = a - b;
     }
+    std::sort( s.begin(), s.end(),  is_larger ); 
 
     //  Main
+    long long sum = 0LL;
+    for( int i = 0; i < K; i ++ ) {
+        sum += ( long long ) s.at( i );
+    }
+
     //  Output results
-    // std::cout << std::endl;
+    std::cout << sum << std::endl;
     
     //  Finalize
-    std::cerr << "Normally terminated." << std::endl;
+    // std::cerr << "Normally terminated." << std::endl;
     return( 0 );
 }
---
+++ typical90/050-StairJump/star-jump.cpp
@@ -0,0 +1,29 @@
+/*
+    050 - Stair Jump（★3）
+        https://atcoder.jp/contests/typical90/tasks/typical90_ax
+        Author: Keitaro Naruse
+        Date:   2021-12-15
+        MIT License
+*/
+
+// # Solution
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    //  Read N and K
+    int N = 0, L = 0; 
+    std::cin >> N >> L;
+    std::cerr << N << " " << L << std::endl;
+
+    //  Main
+    //  Output result
+    // std::cout << sum << std::endl;
+    
+    //  Finalize
+    std::cerr << "Normally terminated." << std::endl;
+    return( 0 );
+}
