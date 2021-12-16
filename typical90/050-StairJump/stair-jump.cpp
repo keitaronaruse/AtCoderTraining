ac25e63ca8fc9c45d8e872c4a05e125124b0e1fc
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-16 18:43:49 +0900

    updated

--- typical90/050-StairJump/stair-jump.cpp
+++ typical90/050-StairJump/stair-jump.cpp
@@ -7,6 +7,10 @@
 */
 
 // # Solution
+// - K = N / L
+// - k = [ K::-1::0 ]
+//   - i = N - k*L
+//   - Find the number of ways to put i among k+1
 
 #include <iostream>
 #include <vector>
@@ -20,10 +24,31 @@ int main()
     std::cerr << N << " " << L << std::endl;
 
     //  Main
+    long long patterns = 0LL;
+    int K = N / L; 
+    for( int k = 0; k <= K; k ++ ) {
+        int i = N - k * L;
+        if( k == 0 ) {
+            //  All 1s, only a singlepattern
+            patterns ++ ;
+            std::cerr << "All 1s pattern: 1"  << std::endl;
+        }
+        else {
+            if( k < i ) {
+                patterns ++ ;
+                std::cerr << "Put " << k << "-Ls among " << i + 1 << " 1-slots"  << std::endl;
+            }
+            else {
+                patterns ++ ;
+                std::cerr << "Put " << i << "-1s among " << k + 1 << " L-slots"  << std::endl;
+            }
+        }
+    }
     //  Output result
-    // std::cout << sum << std::endl;
+    const long long large_prime = 1000000007LL;
+    std::cout << patterns % large_prime << std::endl;
     
     //  Finalize
-    std::cerr << "Normally terminated." << std::endl;
+    // std::cerr << "Normally terminated." << std::endl;
     return( 0 );
 }
