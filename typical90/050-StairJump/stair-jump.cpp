f612ae772c489bbca701eb0bf9b34ea579d70a35
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-17 13:03:52 +0900

    updated

--- typical90/050-StairJump/stair-jump.cpp
+++ typical90/050-StairJump/stair-jump.cpp
@@ -12,6 +12,9 @@
 //   - i = N - k*L
 //   - Find the number of ways to put i among k+1
 
+// - Mange overflow as calculting the combination 
+//   by reducing multipliers by divisors
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -21,20 +24,24 @@ const long long large_prime = 1000000007LL;
 
 /*
     mod_combination()
-        returns modulo by the large prime
-        
+        returns modulo by the large prime      
 */
-long long mod_combination( int n, int k )
+int mod_combination( int n, int k )
 {
-    long long c = 1LL;
+    int c = 1;
 
-    for( int i = n - k + 1; i <= n; i ++ ) {
-        c *= ( long long ) i;
-        c %= large_prime;
+    //  (n, k) == (n, n-k)
+    if( k > n - k ) {
+        k = n - k;
     }
-    for( int j = 1; j <= k; j ++ ) {
-        c /= ( long long ) j;
-    }
+
+    // if( ( k == 0 ) || ( k == n ) ) {
+    //     c = ;
+    // }
+    // else if () {
+    //     ;
+    // }
+
     return( c );
 }
 
@@ -43,38 +50,11 @@ int main()
     //  Read N and K
     int N = 0, L = 0; 
     std::cin >> N >> L;
-    std::cerr << N << " " << L << std::endl;
+    // std::cerr << N << " " << L << std::endl;
 
-    //  Main
-    long long patterns = 0LL;
-    int K = N / L; 
-    for( int k = 0; k <= K; k ++ ) {
-        int i = N - k * L;
-        if( k == 0 ) {
-            //  All 1s, only a singlepattern
-            patterns ++ ;
-            patterns %= large_prime;
-            std::cerr << "All 1s pattern: 1"  << std::endl;
-        }
-        else {
-            if( k < i ) {
-                patterns += mod_combination( i + 1, k );
-                patterns %= large_prime;
-                std::cerr << "Put " << k << "-Ls among " << i + 1 << " 1-slots"  << " " 
-                << mod_combination( i + 1, k ) << std::endl;
-            }
-            else {
-                patterns += mod_combination( k + 1, i ) ;
-                patterns %= large_prime;
-                std::cerr << "Put " << i << "-1s among " << k + 1 << " L-slots"  << " "
-                << mod_combination( k + 1, i ) << std::endl;
-            }
-        }
-    }
-    //  Output result
-    std::cout << patterns << std::endl;
-    
+    std::vector< std::vector< int > > combination( N, std::vector<int>( N / 2 + 1 ) );
+
     //  Finalize
-    // std::cerr << "Normally terminated." << std::endl;
+    std::cerr << "Normally terminated." << std::endl;
     return( 0 );
 }
