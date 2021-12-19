cc9fc211f4e34a44ea48b38e725c66e45fa06417
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-19 18:02:04 +0900

    updated

---
+++ ABC/ABC232/A/abc232-a.cpp
@@ -0,0 +1,26 @@
+/*
+    ABC232 Problem A
+        https://atcoder.jp/contests/abc232
+        Author: Keitaro Naruse
+        Date:   2021-12-19
+        MIT License
+*/
+
+// # Solution
+// - 
+
+#include <iostream>
+
+int main()
+{
+    //  Read N
+    int N = 0;
+    std::cin >> N;
+
+    //  Display result
+    std::cout << std::endl;
+    
+    //  Debug
+    // std::cerr << "Normally terminated." << std::endl;
+    return( 0 );
+}
--- typical90/064-Uplift/uplift.cpp
+++ typical90/064-Uplift/uplift.cpp
@@ -7,6 +7,14 @@
 */
 
 // # Solution
+// - The Uplift does no chage the incovenience in the middle points. 
+// - The inconvenience between Li - 1 and Li, and 
+// - The inconvenience between Ri and Ri + 1
+// - B_L  = E_{ L - 1 } - E_L 
+// - B_L' = E_{ L - 1}  - ( E_L + V_j ) = B_L - V_j
+// - B_R  = E_R - E_{ R + 1 }
+// - B_R' = E_R + V_j - E_{ R + 1 } - E_{ R + 1 }  = B_R + V_j
+
 
 #include <iostream>
 #include <vector>
@@ -23,84 +31,65 @@ int inconvenie( int a, int b )
 int main()
 {
     //  Initialize
-    //  Read a problem
     //  Read N and Q
     int N = 0, Q = 0;
     std::cin >> N >> Q;
-    // std::cerr << N << " "<< Q << std::endl;
     
     //  Read Ai and make Ei
     std::vector< int > A( N, 0 ), E( N, 0 );
     for( int i = 0; i < N; i ++ ) {
         std::cin >> A.at( i );
         E.at( i ) = A.at( i );
-        //  Debug
-        // std::cerr << A.at( i ) << " ";
     }
-    //  Debug
-    // std::cerr << std::endl;
+    //  Calculate the initial inconvenience
+    long long SumInconvenience = 0LL;
+    for( int i = 0; i < N - 1; i ++ ) {
+        SumInconvenience += ( long long ) inconvenie( E.at( i ), E.at( i + 1 ) );
+    }
 
-    //  Read Lj, Rj, Vj
+    //  Main
     std::vector< int > L( Q, 0 ), R( Q, 0 ), V( Q, 0 );
     for( int j = 0; j < Q; j ++ ) {
+        //  Read Lj, Rj, Vj
         std::cin >> L.at( j ) >> R.at( j ) >> V.at( j );
-        //  Debug
-        // std::cerr << L.at( j ) << " " << R.at( j ) << " " << V.at( j ) << std::endl;
-    }
 
-    //  Main
-    //  Calculate the initial inconvenience
-    long long SumInconvenie = 0LL;
-    std::vector< int > InConvenie( N - 1, 0 );
-    for( int i = 0; i < N - 1; i ++ ) {
-        InConvenie.at( i ) = inconvenie( E.at( i ), E.at( i + 1 ) );
-        SumInconvenie += ( long long ) InConvenie.at( i ) ;
-    }
-    //  Debug
-    // for( int i = 0; i < N; i ++ ) {
-    //     std::cerr << E.at( i ) << " ";
-    // }
-    // std::cerr << std::endl;
-    // for( int i = 0; i < N - 1; i ++ ) {
-    //     std::cerr << InConvenie.at( i ) << " ";
-    // }
-    // std::cerr << ": " << SumInconvenie << std::endl;
+        //  Changes of the inconveniences apper at (L-1, L) and (R, R+1)
+        
+        //  L = 1
+        //  E1'=E1 + V, E2'=E2+V, E1'-E2'=E1-E2
+        //  L = 2
+        //  E1'=E1, E_2'=E2+V, E_1'-E2' = E1-E2-V
+        
+        //  R = N
+        //  EN-1'=EN-1+V, EN'=EN+V, EN-1'-EN'=EN-1-EN 
+        //  R = N-1
+        //  EN-1'=EN-1+V, EN'=EN, EN-1'-EN'=EN-1-EN+V 
+        
+        if( L.at( j ) - 2 >=  0) {
+            SumInconvenience 
+            -= ( long long ) inconvenie( E.at( L.at( j ) - 2 ), E.at( L.at( j ) - 1 ) );
+        }
+        if( R.at( j ) < N ) {
+            SumInconvenience 
+            -= ( long long ) inconvenie( E.at( R.at( j ) - 1 ), E.at( R.at( j ) ) );
+        }
 
-    //  Main
-    for( int j = 0; j < Q; j ++ ) {
         //  Update the elevation Ei
         for( int i = L.at( j ) - 1; i <= R.at( j ) - 1; i ++ ) {
             E.at( i ) += V.at( j );
         }
 
-        //  Update inconveniek
-        for( int i = L.at( j ) - 1; i <= R.at( j ) - 1; i ++ ) {
-            if( ( i == L.at( j ) - 1 ) && ( i - 1 >= 0 ) ) {
-                //  Head and the previous exists
-                SumInconvenie -= ( long long ) InConvenie.at( i - 1 ) ;
-                InConvenie.at( i - 1 ) = inconvenie( E.at( i - 1 ), E.at( i ) );
-                SumInconvenie += ( long long ) InConvenie.at( i - 1 ) ;
-            }
-            if( i + 1 < N ) {
-                //  If the next exsits
-                SumInconvenie -= ( long long ) InConvenie.at( i ) ;
-                InConvenie.at( i ) = inconvenie( E.at( i ), E.at( i + 1 ) );
-                SumInconvenie += ( long long ) InConvenie.at( i ) ;
-            }
+        if( L.at( j ) - 2 >=  0) {
+            SumInconvenience 
+            += ( long long ) inconvenie( E.at( L.at( j ) - 2 ), E.at( L.at( j ) - 1 ) );
+        }
+        if( R.at( j ) < N ) {
+            SumInconvenience 
+            += ( long long ) inconvenie( E.at( R.at( j ) - 1 ), E.at( R.at( j ) ) );
         }
 
-        //  Debug
-        // for( int i = 0; i < N; i ++ ) {
-        //     std::cerr << E.at( i ) << " ";
-        // }
-        // std::cerr << std::endl;
-        // for( int i = 0; i < N - 1; i ++ ) {
-        //     std::cerr << InConvenie.at( i ) << " ";
-        // }
-        // std::cerr << ": " << SumInconvenie << std::endl;
-
         //  Display result
-        std::cout << SumInconvenie << std::endl;
+        std::cout << SumInconvenience << std::endl;
     }
     
     //  Finalize
