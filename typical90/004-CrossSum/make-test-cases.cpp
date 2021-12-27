0233a54c9da0158b62a878fcf134dd10fe39b7ec
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-27 17:36:36 +0900

    updated

--- typical90/004-CrossSum/make-test-cases.cpp
+++ typical90/004-CrossSum/make-test-cases.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
 
 //  Debug flag
 const bool Debug = false;
@@ -16,7 +17,7 @@ const bool Debug = false;
 int main( int argc, char* argv[] )
 {
     if( argc != 2 ) {
-        std::cerr << "make-test-cases [1-2]" << std::endl;
+        std::cerr << "make-test-cases [1-3]" << std::endl;
         return( -1 );
     }
 
@@ -42,6 +43,25 @@ int main( int argc, char* argv[] )
         for( int i = 0; i < H; i ++ ) {
             for( int j = 0; j < W; j ++ ) {
                 std::cout << "99 ";
+            }
+            std::cout << std::endl;
+        }
+    }
+    //  Random case
+    else if( argv[1][0] == '3' ) {
+        std::random_device seed_gen;
+        std::default_random_engine engine( seed_gen() );
+
+        std::uniform_int_distribution< int > dist; 
+        dist = std::uniform_int_distribution< int >( 2, 2000 );
+        H = dist( engine ); 
+        W = dist( engine );
+        std::cout << H << " " << W << std::endl;
+
+        dist = std::uniform_int_distribution< int >( 1, 99 );
+        for( int i = 0; i < H; i ++ ) {
+            for( int j = 0; j < W; j ++ ) {
+                std::cout << dist( engine ) << " ";
             }
             std::cout << std::endl;
         }
