0ee92327f22fa5c34af3eb36626979bfe9101b72
keitaronaruse <naruse@u-aizu.ac.jp>
2021-12-29 11:26:26 +0900

    updated

---
+++ typical90/055-Select5/make-test-cases.cpp
@@ -0,0 +1,66 @@
+/*
+    make-test-cases.cpp
+    033 - Not Too Bright
+        https://atcoder.jp/contests/typical90/tasks/typical90_ag
+        Author: Keitaro Naruse
+        Date:   2021-11-28, 2021-12-29
+        MIT License
+*/
+
+#include <iostream>
+#include <random>
+
+//  Debug flag
+const bool Debug = false;
+
+int main( int argc, char* argv[] )
+{
+    if( argc != 2 ) {
+        std::cerr << "make-test-cases [1-3]" << std::endl;
+        return( -1 );
+    }
+
+    //  Minimum case
+    if( argv[1][0] == '1' ) {
+        const int N = 5, P = 1, Q = 0;
+        std::cout << N << " " << P << " " << Q << std::endl;
+        const int A = 0;
+        std::cout << A << std::endl;
+    }
+    //  Minimum case
+    else if( argv[1][0] == '2' ) {
+        const int N = 100, P = 1000000000, Q = 999999999;
+        std::cout << N << " " << P << " " << Q << std::endl;
+        for( int i = 0; i < N; i ++ ) {
+            std::cout << "1000000000" << " ";
+        }
+        std::cout << std::endl;
+    }
+    //  Random case
+    else if( argv[1][0] == '3' ) {
+        std::random_device seed_gen;
+        std::default_random_engine engine( seed_gen() );
+
+        std::uniform_int_distribution< int > dist; 
+        dist = std::uniform_int_distribution< int >( 5, 100 );
+        const int N = dist( engine );
+        dist = std::uniform_int_distribution< int >( 1, 1000000000 );
+        const int P = dist( engine ); 
+        dist = std::uniform_int_distribution< int >( 0, P - 1 );
+        const int Q = dist( engine );
+        std::cout << N << " " << P << " " << Q << std::endl;
+        
+        dist = std::uniform_int_distribution< int >( 0, 1000000000 );
+        for( int i = 0; i < N; i ++ ) {
+            const int A = dist( engine );
+            std::cout << A << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    //  Finalize
+    if( Debug ) {
+        std::cerr << "Normally terminated." << std::endl;
+    }
+    return( 0 );
+}
--- typical90/055-Select5/select5.cpp
+++ typical90/055-Select5/select5.cpp
@@ -2,67 +2,103 @@
     055 - Select 5（★2）
         https://atcoder.jp/contests/typical90/tasks/typical90_bc
         Author: Keitaro Naruse
-        Date:   2021-11-30
+        Date:   2021-11-30, 2021-12-29
         MIT License
 */
 
 // # Solution
-// - Ai = (ki * P + Qi)
-// - Ai*Aj*...*Am 
-//   = (ki * P + Ri)*(kj * P + Rj) *...* (km * P + Rm)
-//   = (...)*P + Ri*Rj*...Rm
-// - Ai*Aj*...*Am % P == Ri*Rj*...Rm % P
+// - Principle
+//   - Ai = (ki * P + Qi)
+//   - Ai*Aj*...*Am 
+//     = (ki * P + Ri)*(kj * P + Rj) *...* (km * P + Rm)
+//     = (...)*P + Ri*Rj*...Rm
+//   - Ai*Aj*...*Am % P == Ri*Rj*...Rm % P
+// - Solution 1: Greedy
+// - Solution 2: DP base
 
 #include <iostream>
 #include <vector>
+
+const bool Debug = false;
+
+/*
+    greedy()
+        returns the solution by the greedy 
+*/
+long long greedy( int N, int P, int Q, const std::vector< int >& R )
+{
+    long long count = 0LL;
+
+    //  Make all the cases of picking up 5 out of N
+    for( int i = 0; i < N; i ++ ) {
+        for( int j = i + 1; j < N; j ++ ) {
+            for( int k = j + 1; k < N; k ++ ) {
+                for( int l = k + 1; l < N; l ++ ) {
+                    for( int m = l + 1; m < N; m ++) {
+                        long long r = ( long long ) R.at( i );
+                        int index[] = { j, k, l, m };
+                        for( int n = 0; n < 4; n ++ ) {
+                            r *= ( long long ) R.at( index[ n ] );
+                            r %= ( long long ) P;
+                        }
+                        if( r == ( long long ) Q ) {
+                            count ++;
+                        }
+                        if( Debug ) {
+                            std::cerr << i << " " << j << " " << k << " " << l << " " << m << std::endl;
+                        } 
+                    }
+                }
+            }
+        }
+    }
+    return( count );
+}
+
+/*
+    dp()
+        returns the solution by the dynamic programming
+        DP table
+                      n = 1, n = 2, ..., n = N 10^2
+        Residue = 0   cases
+        Residue = 1   cases
+        ...
+        Residue < P-1 cases
+        10^9 too long to apply DP, we need a sparce table
+
+*/
 
 int main()
 {
     //  Read N, P, Q
     int N = 0, P = 0, Q = 0;
     std::cin >> N >> P >> Q;
-    //  Debug
-    // std::cerr << N << " " << P << " " << Q << std::endl;
+    if( Debug ) {
+        std::cerr << N << " " << P << " " << Q << std::endl;
+    }
     
     //  Read Ai and make Ri
     std::vector< int > A( N );
     std::vector< int > R( N );
-    for( int i = 0; i != N; i ++ ) {
+    for( int i = 0; i < N; i ++ ) {
         std::cin >> A.at( i );
         R.at( i ) = A.at( i ) % P;
-        //  Debug
-        // std::cerr << A.at( i ) << ":" << R.at( i ) << " ";
-    }
-    //  Debug
-    // std::cerr << std::endl;
-
-    //  Make all the patterns of picking up 5 out of N
-    int count = 0;
-    for( int i = 0; i != N; i ++ ) {
-        for( int j = i + 1; j != N; j ++ ) {
-            for( int k = j + 1; k != N; k ++ ) {
-                for( int l = k + 1; l != N; l ++ ) {
-                    for( int m = l + 1; m != N; m ++) {
-                        int index[] = { i, j, k, l, m }; 
-                        //  Debug
-                        // std::cerr << index[0] << " " << index[1] << " " << index[2] << " " << index[3] << " " << index[4] << std::endl;
-                        long long r = (long long) R.at( index[ 0 ] ) ;
-                        for( int n = 1; n != 5; n ++ ) {
-                            r = ( r * (long long) R.at( index[ n ] ) ) % (long long) P;
-                        }
-                        if( r == (long long) Q ) {
-                            count ++;
-                        }
-                    }
-                }
-            }
+        if( Debug ) {
+            std::cerr << A.at( i ) << ":" << R.at( i ) << " ";
         }
     }
+    if( Debug ) {
+        std::cerr << std::endl;
+    }
+
+    long long count = greedy( N, P, Q, R ); 
 
     //  Display result
     std::cout << count << std::endl;
     
     //  Finalize
-    // std::cerr << "Normally terminated." << std::endl;
+    if( Debug ) {
+        std::cerr << "Normally terminated." << std::endl;        ;
+    }
     return( 0 );
 }
