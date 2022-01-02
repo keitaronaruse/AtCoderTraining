4324f473342b4278a7ff3ea9f3157d67f6a52479
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-02 21:36:20 +0900

    updated

--- nrs/adj-graph.cpp
+++ nrs/adj-graph.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <stack>
 
 namespace nrs {
     class adj_graph {
@@ -28,6 +29,56 @@ namespace nrs {
             bool has_edge( int b, int e ) {
                 return( ( bool ) adj_nodes.at( b ).count( e ) );
             }
+            int length_dfs( int b, int e ) {
+                std::stack< int > s;
+                std::vector< int > length( n, -1 );
+                
+                length.at( b ) = 0;
+                s.push( b );
+                while( !s.empty() ) {
+                    int v = s.top();
+                    s.pop();
+                    if( v == e ) {
+                        break;
+                    }
+                    else {
+                        for( auto u : adj_nodes.at( v ) ) {
+                            if( length.at( u ) == -1 ) {
+                                length.at( u ) = length.at( v ) + 1;
+                                s.push( u );
+                            }
+                            else if( length.at( u ) > length.at( v ) + 1 ) {
+                                length.at( u ) = length.at( v ) + 1;
+                                s.push( u );
+                            }
+                        }
+                    }
+                }
+                return( length.at( e ) );
+            }
+            bool is_connected_dfs( int b, int e ) {
+                std::stack< int > s;
+                std::vector< bool > visited( n, false );
+                
+                visited.at( b ) = true;
+                s.push( b );
+                while( !s.empty() ) {
+                    int v = s.top();
+                    s.pop();
+                    if( v == e ) {
+                        return( true );
+                    }
+                    else {
+                        for( auto u : adj_nodes.at( v ) ) {
+                            if( !visited.at( u ) ) {
+                                visited.at( u ) = true;
+                                s.push( u );
+                            }
+                        }
+                    }
+                }
+                return( false );
+            }
         public:
             int n;
             std::vector< std::set< int > > adj_nodes;
@@ -40,19 +91,17 @@ int main()
     nrs::adj_graph g( 3 );
 
     std::cerr << "Add the edge( 0, 1 )" << std::endl;
-    g.add_edge( 0, 1 );
-    
+    g.add_edge( 0, 1 ); g.add_edge( 1, 0 );
     std::cerr << "Add the edge( 1, 2 )" << std::endl;
-    g.add_edge( 1, 2 );
+    g.add_edge( 1, 2 ); g.add_edge( 2, 1 );
 
-    std::cerr << "Has the edge( 0, 1 )? " 
-    << g.has_edge( 0, 1 ) << std::endl;
+    std::cerr << "Length of ( 0, 0 )? " << g.length_dfs( 0, 0 ) << std::endl;
+    std::cerr << "Length of ( 0, 1 )? " << g.length_dfs( 0, 1 ) << std::endl;
+    std::cerr << "Length of ( 0, 2 )? " << g.length_dfs( 0, 2 ) << std::endl;
 
-    std::cerr << "Has the edge( 1, 2 )? " 
-    << g.has_edge( 1, 2 ) << std::endl;
-
-    std::cerr << "Has the edge( 2, 0 )? " 
-    << g.has_edge( 2, 0 ) << std::endl;
+    std::cerr << "Add the edge( 0, 2 )" << std::endl;
+    g.add_edge( 0, 2 ); g.add_edge( 2, 0 );
+    std::cerr << "Length of ( 0, 2 )? " << g.length_dfs( 0, 2 ) << std::endl;
 
     return( 0 );
 }
--- typical90/064-Uplift/uplift.cpp
+++ typical90/064-Uplift/uplift.cpp
@@ -2,7 +2,7 @@
     064 - Uplift（★3）
         https://atcoder.jp/contests/typical90/tasks/typical90_bl
         Author: Keitaro Naruse
-        Date:   2021-12-19, 2021-12-21 
+        Date:   2021-12-19, 2022-01-02 
         MIT License
 */
 
@@ -16,9 +16,14 @@
 #include <iostream>
 #include <vector>
 
-long long my_abs( long long a )
-{
-    return( ( a > 0 )? a : -a );
+const bool Debug = false;
+
+namespace nrs {
+    template < class T >
+    T abs( T a )
+    {
+        return( ( a > 0 )? a : -a );
+    }
 }
 
 int main()
@@ -27,59 +32,64 @@ int main()
     //  Read N and Q
     int N = 0, Q = 0;
     std::cin >> N >> Q;
+    if( Debug ) {
+        std::cerr << N << " " << Q << std::endl;
+    }
     
     //  Read Ai and put to Ei
     std::vector< long long > E( N, 0 );
     for( int i = 0; i < N; i ++ ) {
-        std::cin >> E.at( i );
+        int A = 0;
+        std::cin >> A;
+        E.at( i ) = A;
+        if( Debug ) {
+            std::cerr << A << " ";
+        }
     }
+    if( Debug ) {
+        std::cerr << std::endl;
+    }
+
     //  Calculate Bi, the difference of Ei
     //  Calculate the initial inconvenience
     long long SumInconvenience = 0LL;
     std::vector< long long > B( N - 1, 0 );
     for( int i = 0; i < N - 1; i ++ ) {
         B.at( i ) = E.at( i ) - E.at( i + 1 );
-        SumInconvenience += my_abs( B.at( i ) );
+        SumInconvenience += nrs::abs( B.at( i ) );
     }
 
     //  Main
     for( int j = 0; j < Q; j ++ ) {
-        int L = 0, R = 0, V = 0;
         //  Read Lj, Rj, Vj
+        int L = 0, R = 0, V = 0;
         std::cin >> L >> R >> V;
+        if( Debug ) {
+            std::cerr << L << " " << R << " " << V << std::endl;
+        }
         L --; 
         R --;
-        //  Debug
-        // std::cerr << L << " " << R << " " << V << std::endl;
 
         //  Main
         //  The changes of the inconveniences apper at B[L-1] and B[R]
         if( L - 1 >= 0 ) {
-            SumInconvenience -= my_abs( B.at( L - 1 ) );
-            B.at( L - 1 ) -= V;
-            SumInconvenience += my_abs( B.at( L - 1 ) );
+            SumInconvenience -= nrs::abs( B.at( L - 1 ) );
+            B.at( L - 1 ) -= ( long long ) V;
+            SumInconvenience += nrs::abs( B.at( L - 1 ) );
         }
         if( R + 1 < N ) {
-            SumInconvenience -= my_abs( B.at( R ) );
-            B.at( R ) += V;
-            SumInconvenience += my_abs( B.at( R ) );
+            SumInconvenience -= nrs::abs( B.at( R ) );
+            B.at( R ) += ( long long ) V;
+            SumInconvenience += nrs::abs( B.at( R ) );
         }
         
         //  Display result
         std::cout << SumInconvenience << std::endl;
-
-        //  Debug
-        // for( int i = 0; i < N; i ++ ) {
-        //     std::cerr << E.at( i ) << " ";
-        // }
-        // std::cerr << std::endl;
-        // for( int i = 0; i < N - 1; i ++ ) {
-        //     std::cerr << B.at( i ) << " ";
-        // }
-        // std::cerr << ": " << SumInconvenience << std::endl;
     }
     
     //  Finalize
-    // std::cerr << "Normally terminated." << std::endl;
+    if( Debug ) {
+        std::cerr << "Normally terminated." << std::endl;
+    }
     return( 0 );
 }
