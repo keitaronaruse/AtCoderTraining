d130b75fabf60537e57851dc6126fd348baab99e
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-03 08:12:11 +0900

    updated

--- nrs/adj-graph.cpp
+++ nrs/adj-graph.cpp
@@ -10,16 +10,17 @@
 #include <vector>
 #include <set>
 #include <stack>
+#include <queue>
 
 namespace nrs {
     class adj_graph {
         public:
             adj_graph() : n( 0 ) {
                 adj_nodes = std::vector< std::set< int > >();
-            };
+            }
             adj_graph( int N ) : n( N ) {
                 adj_nodes = std::vector< std::set< int > >( N );
-            };
+            }
             adj_graph( const adj_graph& r ) : n( r.n ) {
                 adj_nodes = r.adj_nodes;
             }
@@ -56,28 +57,61 @@ namespace nrs {
                 }
                 return( length.at( e ) );
             }
-            bool is_connected_dfs( int b, int e ) {
-                std::stack< int > s;
-                std::vector< bool > visited( n, false );
+            int length_bfs( int b, int e ) {
+                std::queue< int > s;
+                std::vector< int > length( n, -1 );
                 
-                visited.at( b ) = true;
+                length.at( b ) = 0;
                 s.push( b );
                 while( !s.empty() ) {
-                    int v = s.top();
+                    int v = s.front();
                     s.pop();
                     if( v == e ) {
-                        return( true );
+                        break;
                     }
                     else {
                         for( auto u : adj_nodes.at( v ) ) {
-                            if( !visited.at( u ) ) {
-                                visited.at( u ) = true;
+                            if( length.at( u ) == -1 ) {
+                                length.at( u ) = length.at( v ) + 1;
+                                s.push( u );
+                            }
+                            else if( length.at( u ) > length.at( v ) + 1 ) {
+                                length.at( u ) = length.at( v ) + 1;
                                 s.push( u );
                             }
                         }
                     }
                 }
-                return( false );
+                return( length.at( e ) );
+            }
+            int find_longest_path_from_bfs( int b, int& e ) {
+                std::queue< int > s;
+                std::vector< int > length( n, -1 );
+                int max_length = -1;
+
+                max_length = length.at( b ) = 0;
+                e = b;
+                s.push( b );
+                while( !s.empty() ) {
+                    int v = s.front();
+                    s.pop();
+                    if( max_length < length.at( v ) ) {
+                        max_length = length.at( v );
+                        e = v;
+                    }
+
+                    for( auto u : adj_nodes.at( v ) ) {
+                        if( length.at( u ) == -1 ) {
+                            length.at( u ) = length.at( v ) + 1;
+                            s.push( u );
+                        }
+                        else if( length.at( u ) > length.at( v ) + 1 ) {
+                            length.at( u ) = length.at( v ) + 1;
+                            s.push( u );
+                        }
+                    }
+                }
+                return( length.at( e ) );
             }
         public:
             int n;
@@ -90,18 +124,42 @@ int main()
 {
     nrs::adj_graph g( 3 );
 
-    std::cerr << "Add the edge( 0, 1 )" << std::endl;
+    std::cerr << "Add the edge ( 0, 1 )" << std::endl;
     g.add_edge( 0, 1 ); g.add_edge( 1, 0 );
-    std::cerr << "Add the edge( 1, 2 )" << std::endl;
+    std::cerr << "Add the edge ( 1, 2 )" << std::endl;
     g.add_edge( 1, 2 ); g.add_edge( 2, 1 );
 
-    std::cerr << "Length of ( 0, 0 )? " << g.length_dfs( 0, 0 ) << std::endl;
-    std::cerr << "Length of ( 0, 1 )? " << g.length_dfs( 0, 1 ) << std::endl;
-    std::cerr << "Length of ( 0, 2 )? " << g.length_dfs( 0, 2 ) << std::endl;
+    std::cerr << "Length of ( 0, 0 ) by DFS: " << g.length_dfs( 0, 0 ) << std::endl;
+    std::cerr << "Length of ( 0, 0 ) by BFS: " << g.length_bfs( 0, 0 ) << std::endl;
+    std::cerr << "Length of ( 0, 1 ) by DFS: " << g.length_dfs( 0, 1 ) << std::endl;
+    std::cerr << "Length of ( 0, 1 ) by BFS: " << g.length_bfs( 0, 1 ) << std::endl;
+    std::cerr << "Length of ( 0, 2 ) by DFS: " << g.length_dfs( 0, 2 ) << std::endl;
+    std::cerr << "Length of ( 0, 2 ) by BFS: " << g.length_bfs( 0, 2 ) << std::endl;
 
-    std::cerr << "Add the edge( 0, 2 )" << std::endl;
+    int length = 0, e = 0;
+    length = g.find_longest_path_from_bfs( 0, e );
+    std::cerr << "Longest node from ( 0 ) by BFS: " << e << " with " << length << std::endl;
+    length = g.find_longest_path_from_bfs( 1, e );
+    std::cerr << "Longest node from ( 1 ) by BFS: " << e << " with " << length << std::endl;
+    length = g.find_longest_path_from_bfs( 2, e );
+    std::cerr << "Longest node from ( 2 ) by BFS: " << e << " with " << length << std::endl;
+
+    std::cerr << "Add the edge ( 0, 2 )" << std::endl;
     g.add_edge( 0, 2 ); g.add_edge( 2, 0 );
-    std::cerr << "Length of ( 0, 2 )? " << g.length_dfs( 0, 2 ) << std::endl;
+
+    std::cerr << "Length of ( 0, 0 ) by DFS: " << g.length_dfs( 0, 0 ) << std::endl;
+    std::cerr << "Length of ( 0, 0 ) by BFS: " << g.length_bfs( 0, 0 ) << std::endl;
+    std::cerr << "Length of ( 0, 1 ) by DFS: " << g.length_dfs( 0, 1 ) << std::endl;
+    std::cerr << "Length of ( 0, 1 ) by BFS: " << g.length_bfs( 0, 1 ) << std::endl;
+    std::cerr << "Length of ( 0, 2 ) by DFS: " << g.length_dfs( 0, 2 ) << std::endl;
+    std::cerr << "Length of ( 0, 2 ) by BFS: " << g.length_bfs( 0, 2 ) << std::endl;
+
+    length = g.find_longest_path_from_bfs( 0, e );
+    std::cerr << "Longest node from ( 0 ) by BFS: " << e << " with " << length << std::endl;
+    length = g.find_longest_path_from_bfs( 1, e );
+    std::cerr << "Longest node from ( 1 ) by BFS: " << e << " with " << length << std::endl;
+    length = g.find_longest_path_from_bfs( 2, e );
+    std::cerr << "Longest node from ( 2 ) by BFS: " << e << " with " << length << std::endl;
 
     return( 0 );
 }
