51caf9a7b7559e26c41219d5e52ecdd37b27acd4
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-06 15:14:10 +0900

    updated

--- typical90/026-IndependentSetOnATree/independent-set.cpp
+++ typical90/026-IndependentSetOnATree/independent-set.cpp
@@ -7,185 +7,54 @@
 */
 
 // # Solution
+// - Make a tree and sweep a tree by DFS
+// - Assign an alternative color in a found order
 
 #include <iostream>
 #include <vector>
-#include <set>
 #include <stack>
-#include <queue>
 
-namespace nrs {
-    class adj_graph {
-        public:
-            adj_graph() : n( 0 ) {
-                adj_nodes = std::vector< std::set< int > >();
-            }
-            adj_graph( int N ) : n( N ) {
-                adj_nodes = std::vector< std::set< int > >( N );
-            }
-            adj_graph( const adj_graph& r ) : n( r.n ) {
-                adj_nodes = r.adj_nodes;
-            }
-            void add_edge( int b, int e ) {
-                adj_nodes.at( b ).insert( e );
-            }
-            bool has_edge( int b, int e ) {
-                return( ( bool ) adj_nodes.at( b ).count( e ) );
-            }
-            /*
-                length_dfs()
-                    returns a path length from b to e by the depth first search
-                    if e == -1, stores the farset node from b in e and returns the diameter
-            */
-            int length_dfs( int b, int& e ) {
-                std::stack< int > s;
-                length = std::vector< int >( n, -1 );
-                int max_length = length.at( b ) = 0;
-                int max_node = e;
-                
-                s.push( b );
-                while( !s.empty() ) {
-                    int v = s.top(); s.pop();
-                    //  Mode of finding a path length from b to the farest node e
-                    if( e == -1 ) {
-                        if( max_length < length.at( v ) ) {
-                            max_length = length.at( v );
-                            max_node = v;
-                        }
+class color_tree  {
+public:
+    int n;
+    std::vector< std::vector< int > > adj_nodes;
+    std::vector< int > length;
+    std::vector< int > even, odd;
+public:
+    color_tree( int N ) : n( N ) {
+        adj_nodes = std::vector< std::vector< int > >( N );
+    }
+    void add_edge( int b, int e ) {
+        adj_nodes.at( b ).push_back( e );
+    }
+    void color_dfs( int b ) {
+        std::stack< int > s;
+        length = std::vector< int >( n, -1 );
+        
+        length.at( b ) = 0;
+        even.push_back( b );
+        s.push( b );
+        while( !s.empty() ) {
+            int v = s.top(); 
+            s.pop();
+            //  Common procedure
+            for( auto u : adj_nodes.at( v ) ) {
+                if( length.at( u ) == -1 ) {
+                    length.at( u ) = ( length.at( v ) + 1 ) % 2;
+                    if( length.at( u ) == 0 ) {
+                        even.push_back( u );
                     }
-                    //  Mode of finding a path length from b to e
                     else {
-                        if( v == e ) {
-                            break;
-                        }
+                        odd.push_back( u );
                     }
-                    //  Common procedure
-                    for( auto u : adj_nodes.at( v ) ) {
-                        if( length.at( u ) == -1 ) {
-                            length.at( u ) = length.at( v ) + 1;
-                            s.push( u );
-                        }
-                        else if( length.at( u ) > length.at( v ) + 1 ) {
-                            length.at( u ) = length.at( v ) + 1;
-                            s.push( u );
-                        }
-                    }
+                    s.push( u );
                 }
-                if( e == -1 ) {
-                    e = max_node;
-                }
-                return( length.at( e ) );
             }
-            /*
-                length_bfs()
-                    returns a path length from b to e by the breadth first search
-                    if e == -1, stores the farset node from b in e and returns the diameter
-            */
-            int length_bfs( int b, int& e ) {
-                std::queue< int > s;
-                length = std::vector< int >( n, -1 );
-                int max_length = length.at( b ) = 0;
-                int max_node = e;
-                
-                s.push( b );
-                while( !s.empty() ) {
-                    int v = s.front(); s.pop();
-                    //  Mode of finding a path length from b to the farest node e
-                    if( e == -1 ) {
-                        if( max_length < length.at( v ) ) {
-                            max_length = length.at( v );
-                            max_node = v;
-                        }
-                    }
-                    //  Mode of finding a path length from b to e
-                    else {
-                        if( v == e ) {
-                            break;
-                        }
-                    }
-                    //  Common procedure
-                    for( auto u : adj_nodes.at( v ) ) {
-                        if( length.at( u ) == -1 ) {
-                            length.at( u ) = length.at( v ) + 1;
-                            s.push( u );
-                        }
-                        else if( length.at( u ) > length.at( v ) + 1 ) {
-                            length.at( u ) = length.at( v ) + 1;
-                            s.push( u );
-                        }
-                    }
-                }
-                if( e == -1 ) {
-                    e = max_node;
-                }
-                return( length.at( e ) );
-            }
-        public:
-            int n;
-            std::vector< std::set< int > > adj_nodes;
-            std::vector< int > length;
-    };
-}
-
-<<<<<<< HEAD
-class color_tree : public nrs::adj_graph
-{
-    public:
-        std::vector< int > even, odd;
-
-        color_tree( int N ) : adj_graph( N ) {
-            even = std::vector< int >( N );
         }
-            /*
-                length_dfs()
-                    returns a path length from b to e by the depth first search
-                    if e == -1, stores the farset node from b in e and returns the diameter
-            */
-            int length_dfs( int b, int& e ) {
-                std::stack< int > s;
-                length = std::vector< int >( n, -1 );
-                int max_length = length.at( b ) = 0;
-                int max_node = e;
-                
-                s.push( b );
-                while( !s.empty() ) {
-                    int v = s.top(); s.pop();
-                    //  Mode of finding a path length from b to the farest node e
-                    if( e == -1 ) {
-                        if( max_length < length.at( v ) ) {
-                            max_length = length.at( v );
-                            max_node = v;
-                        }
-                    }
-                    //  Mode of finding a path length from b to e
-                    else {
-                        if( v == e ) {
-                            break;
-                        }
-                    }
-                    //  Common procedure
-                    for( auto u : adj_nodes.at( v ) ) {
-                        if( length.at( u ) == -1 ) {
-                            length.at( u ) = length.at( v ) + 1;
-                            s.push( u );
-                        }
-                        else if( length.at( u ) > length.at( v ) + 1 ) {
-                            length.at( u ) = length.at( v ) + 1;
-                            s.push( u );
-                        }
-                    }
-                }
-                if( e == -1 ) {
-                    e = max_node;
-                }
-                return( length.at( e ) );
-            }
+    }
 };
 
-const bool Debug = true;
-=======
 const bool Debug = false;
->>>>>>> 488304a6eee863d4f0b097910ac47a4fa31d4990
 
 int main()
 {
@@ -197,7 +66,6 @@ int main()
     }
 
     //  Make a graph instance
-    nrs::adj_graph g( N );
     color_tree t( N );
     
     //  Read A and B
@@ -208,41 +76,43 @@ int main()
             std::cerr << A << " " << B << std::endl;
         }
         A --; B --;
-        g.add_edge( A, B ); g.add_edge( B, A );
+        t.add_edge( A, B ); 
+        t.add_edge( B, A );
     }
 
     //  Main
-    int b = 0, e = -1;
-    g.length_dfs( b, e );
+    int b = 0;
+    t.color_dfs( b );
+    
     if( Debug ) {
-        for( auto v : g.length ) {
+        for( auto v : t.length ) {
+            std::cerr << v << " ";
+        }
+        std::cerr << std::endl;
+        for( auto v : t.even ) {
+            std::cerr << v << " ";
+        }
+        std::cerr << std::endl;
+        for( auto v : t.odd ) {
             std::cerr << v << " ";
         }
         std::cerr << std::endl;
     }
 
-    std::vector< int > even, odd;
-    for( int i = 0; i < N; i ++ ) {
-        if ( g.length.at( i ) % 2 == 0 ) {
-            even.push_back( i );
-        }
-        else {
-            odd.push_back( i );
-        }
-    }
-
-    if( even.size() > odd.size() ) {
+    //  Display results
+    if( t.even.size() > t.odd.size() ) {
         for( int i = 0; i < N / 2; i ++ ) {
-            std::cout << even.at( i ) + 1 << " ";
+            std::cout << t.even.at( i ) + 1 << " ";
         }
         std::cout << std::endl;
     }
     else {
         for( int i = 0; i < N / 2; i ++ ) {
-            std::cout << odd.at( i ) + 1 << " ";
+            std::cout << t.odd.at( i ) + 1 << " ";
         }
         std::cout << std::endl;
     }
+
     //  Finalize
     if( Debug ) {
         std::cerr << "Normally terminated." << std::endl;
