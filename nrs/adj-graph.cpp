cdd21eecdffeb10a032932d359b1bd3d0c5557d6
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-06 11:56:15 +0900

    updated

--- nrs/adj-graph.cpp
+++ nrs/adj-graph.cpp
@@ -2,7 +2,7 @@
     nrs::adj_graph
         The class of a graph structure by adjacent nodes 
         Author: Keitaro Naruse
-        Date:   2021-12-31, 2022-01-05
+        Date:   2021-12-31, 2022-01-06
         MIT License
 */
 
@@ -33,29 +33,31 @@ namespace nrs {
             /*
                 length_dfs()
                     returns a path length from b to e by the depth first search
-                    if e == -1, finds the farset node from b and returns the diameter
+                    if e == -1, stores the farset node from b in e and returns the diameter
             */
-            int length_dfs( int b, int e ) {
+            int length_dfs( int b, int& e ) {
                 std::stack< int > s;
                 length = std::vector< int >( n, -1 );
                 int max_length = length.at( b ) = 0;
-                int max_node = -1;
+                int max_node = e;
                 
                 s.push( b );
                 while( !s.empty() ) {
                     int v = s.top(); s.pop();
-
-                    }
+                    //  Mode of finding a path length from b to the farest node e
                     if( e == -1 ) {
                         if( max_length < length.at( v ) ) {
                             max_length = length.at( v );
                             max_node = v;
+                        }
                     }
+                    //  Mode of finding a path length from b to e
                     else {
                         if( v == e ) {
                             break;
                         }
                     }
+                    //  Common procedure
                     for( auto u : adj_nodes.at( v ) ) {
                         if( length.at( u ) == -1 ) {
                             length.at( u ) = length.at( v ) + 1;
@@ -74,22 +76,32 @@ namespace nrs {
             }
             /*
                 length_bfs()
-                    returns a path length from b to e
-                    if e == -1, finds the farset node and returns the diameter
+                    returns a path length from b to e by the breadth first search
+                    if e == -1, stores the farset node from b in e and returns the diameter
             */
-            int length_bfs( int b, int e ) {
+            int length_bfs( int b, int& e ) {
                 std::queue< int > s;
                 length = std::vector< int >( n, -1 );
                 int max_length = length.at( b ) = 0;
+                int max_node = e;
                 
-                length.at( b ) = 0;
                 s.push( b );
                 while( !s.empty() ) {
-                    int v = s.front();
-                    s.pop();
-                    if( v == e ) {
-                        break;
+                    int v = s.front(); s.pop();
+                    //  Mode of finding a path length from b to the farest node e
+                    if( e == -1 ) {
+                        if( max_length < length.at( v ) ) {
+                            max_length = length.at( v );
+                            max_node = v;
+                        }
                     }
+                    //  Mode of finding a path length from b to e
+                    else {
+                        if( v == e ) {
+                            break;
+                        }
+                    }
+                    //  Common procedure
                     for( auto u : adj_nodes.at( v ) ) {
                         if( length.at( u ) == -1 ) {
                             length.at( u ) = length.at( v ) + 1;
@@ -101,33 +113,8 @@ namespace nrs {
                         }
                     }
                 }
-                return( length.at( e ) );
-            }
-            int find_longest_path_from_bfs( int b, int& e ) {
-                std::queue< int > s;
-                length = std::vector< int >( n, -1 );
-                int max_length = length.at( b ) = 0;
-                
-                e = b;
-                s.push( b );
-                while( !s.empty() ) {
-                    int v = s.front();
-                    s.pop();
-                    if( max_length < length.at( v ) ) {
-                        max_length = length.at( v );
-                        e = v;
-                    }
-
-                    for( auto u : adj_nodes.at( v ) ) {
-                        if( length.at( u ) == -1 ) {
-                            length.at( u ) = length.at( v ) + 1;
-                            s.push( u );
-                        }
-                        else if( length.at( u ) > length.at( v ) + 1 ) {
-                            length.at( u ) = length.at( v ) + 1;
-                            s.push( u );
-                        }
-                    }
+                if( e == -1 ) {
+                    e = max_node;
                 }
                 return( length.at( e ) );
             }
@@ -143,42 +130,82 @@ int main()
 {
     nrs::adj_graph g( 3 );
 
+    //  Make a graph
     std::cerr << "Add the edge ( 0, 1 )" << std::endl;
     g.add_edge( 0, 1 ); g.add_edge( 1, 0 );
     std::cerr << "Add the edge ( 1, 2 )" << std::endl;
     g.add_edge( 1, 2 ); g.add_edge( 2, 1 );
 
-    std::cerr << "Length of ( 0, 0 ) by DFS: " << g.length_dfs( 0, 0 ) << std::endl;
-    std::cerr << "Length of ( 0, 0 ) by BFS: " << g.length_bfs( 0, 0 ) << std::endl;
-    std::cerr << "Length of ( 0, 1 ) by DFS: " << g.length_dfs( 0, 1 ) << std::endl;
-    std::cerr << "Length of ( 0, 1 ) by BFS: " << g.length_bfs( 0, 1 ) << std::endl;
-    std::cerr << "Length of ( 0, 2 ) by DFS: " << g.length_dfs( 0, 2 ) << std::endl;
-    std::cerr << "Length of ( 0, 2 ) by BFS: " << g.length_bfs( 0, 2 ) << std::endl;
+    //  Variables
+    int length = 0, b = 0, e = 0;
+    
+    //  Find a path lenght from b to e by DFS
+    b = 0; e = 0; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Length from " << b << " to " << e << " is " << length << std::endl; 
+    b = 0; e = 1; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Length from " << b << " to " << e << " is " << length << std::endl; 
+    b = 0; e = 2; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Length from " << b << " to " << e << " is " << length << std::endl; 
 
-    int length = 0, e = 0;
-    length = g.find_longest_path_from_bfs( 0, e );
-    std::cerr << "Longest node from ( 0 ) by BFS: " << e << " with " << length << std::endl;
-    length = g.find_longest_path_from_bfs( 1, e );
-    std::cerr << "Longest node from ( 1 ) by BFS: " << e << " with " << length << std::endl;
-    length = g.find_longest_path_from_bfs( 2, e );
-    std::cerr << "Longest node from ( 2 ) by BFS: " << e << " with " << length << std::endl;
+    //  Find a path lenght from b to e by BFS
+    b = 0; e = 0; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Length from " << b << " to " << e << " is " << length << std::endl; 
+    b = 0; e = 1; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Length from " << b << " to " << e << " is " << length << std::endl; 
+    b = 0; e = 2; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Length from " << b << " to " << e << " is " << length << std::endl; 
 
+    //  Find the farest node and its path lenght by DFS
+    b = 0; e = -1; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+    b = 1; e = -1; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+    b = 2; e = -1; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+
+    //  Find the farest node and its path lenght by BFS
+    b = 0; e = -1; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+    b = 1; e = -1; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+    b = 2; e = -1; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+
+    //  Modify the graph
     std::cerr << "Add the edge ( 0, 2 )" << std::endl;
     g.add_edge( 0, 2 ); g.add_edge( 2, 0 );
 
-    std::cerr << "Length of ( 0, 0 ) by DFS: " << g.length_dfs( 0, 0 ) << std::endl;
-    std::cerr << "Length of ( 0, 0 ) by BFS: " << g.length_bfs( 0, 0 ) << std::endl;
-    std::cerr << "Length of ( 0, 1 ) by DFS: " << g.length_dfs( 0, 1 ) << std::endl;
-    std::cerr << "Length of ( 0, 1 ) by BFS: " << g.length_bfs( 0, 1 ) << std::endl;
-    std::cerr << "Length of ( 0, 2 ) by DFS: " << g.length_dfs( 0, 2 ) << std::endl;
-    std::cerr << "Length of ( 0, 2 ) by BFS: " << g.length_bfs( 0, 2 ) << std::endl;
+    //  Find a path lenght from b to e by DFS
+    b = 0; e = 0; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Length from " << b << " to " << e << " is " << length << std::endl; 
+    b = 0; e = 1; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Length from " << b << " to " << e << " is " << length << std::endl; 
+    b = 0; e = 2; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Length from " << b << " to " << e << " is " << length << std::endl; 
 
-    length = g.find_longest_path_from_bfs( 0, e );
-    std::cerr << "Longest node from ( 0 ) by BFS: " << e << " with " << length << std::endl;
-    length = g.find_longest_path_from_bfs( 1, e );
-    std::cerr << "Longest node from ( 1 ) by BFS: " << e << " with " << length << std::endl;
-    length = g.find_longest_path_from_bfs( 2, e );
-    std::cerr << "Longest node from ( 2 ) by BFS: " << e << " with " << length << std::endl;
+    //  Find a path lenght from b to e by BFS
+    b = 0; e = 0; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Length from " << b << " to " << e << " is " << length << std::endl; 
+    b = 0; e = 1; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Length from " << b << " to " << e << " is " << length << std::endl; 
+    b = 0; e = 2; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Length from " << b << " to " << e << " is " << length << std::endl; 
+
+    //  Find the farest node and its path lenght by DFS
+    b = 0; e = -1; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+    b = 1; e = -1; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+    b = 2; e = -1; length = g.length_dfs( b, e );
+    std::cerr << "DFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+
+    //  Find the farest node and its path lenght by BFS
+    b = 0; e = -1; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+    b = 1; e = -1; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
+    b = 2; e = -1; length = g.length_bfs( b, e );
+    std::cerr << "BFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
 
     return( 0 );
 }
