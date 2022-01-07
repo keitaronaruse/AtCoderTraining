127889b3c0b75321172974345f4e6d2a74614421
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-08 08:38:09 +0900

    updated

--- typical90/034-ThereAreFewTypesOfElements/there-are-few.cpp
+++ typical90/034-ThereAreFewTypesOfElements/there-are-few.cpp
@@ -2,16 +2,18 @@
     034 - There are few types of elements（★4）
         https://atcoder.jp/contests/typical90/tasks/typical90_ah
         Author: Keitaro Naruse
-        Date:   2022-01-07
+        Date:   2022-01-07, 2022-01-01
         MIT License
 */
 
 // # Solution
-// - Caliculate by IMOS method
+// - Make Ci: a table of the length of intervals of K different values starting at i
+// - Find the length by the two pointer method
 
 #include <iostream>
 #include <vector>
 #include <map>
+#include <utility>
 
 const bool Debug = true;
 
@@ -24,23 +26,57 @@ int main()
         std::cerr << N << " " << K << std::endl;
     }
 
-    //  Main
+    //  Read Ai
     std::vector< int > A( N );
-    std::map< int, int > C;
     for( int i = 0; i < N; i ++ ) {
         std::cin >> A.at( i );
         if( Debug ) {
             std::cerr << A.at( i ) << " ";
         }
-        C[ A.at(i) ] ++;
     }
     if( Debug ) {
         std::cerr << std::endl;
     }
 
+    //  Main
+    //  Make Ci
+    std::vector< int > C( N );
+    //  A counter of the number and the appearance time 
+    std::map< int, int > counter;
+    int tail = 0;
+    for( int head = 0; head < N; head ++ ) {
+        //  Add the value to the counter
+        if( Debug ) {
+            std::cerr << "Add: " << A.at( tail ) << std::endl;
+        }
+        counter[ A.at( tail ) ] ++;
+        if( tail < N - 1 ) {
+            tail ++;
+        }
+        //  When it is out of the interval
+        if( counter.size() >= K ) {
+            //  Update Ci
+            C.at( head ) = tail;
+            if( Debug ) {
+                std::cerr << "Erase: " << A.at( head ) << std::endl;
+            }
+            //  Decrease the head element and erase it if necesarry 
+            counter[ A.at( head ) ] --;
+            if( counter[ A.at( head ) ] == 0 ) {
+                counter.erase( A.at( head ) );
+            }
+        }
+
+        if( Debug ) {
+            for( auto c: counter ) {
+                std::cerr << head << ": " << c.first << ", " << c.second << std::endl;
+            }
+        }
+    }
+    
     if( Debug ) {
-        for( auto c : C ) {
-            std::cerr << c.first << " " << c.second << std::endl;
+        for( int i = 0; i < N; i ++ ) {
+            std::cerr << i << ": " << C.at( i ) << std::endl;
         }
     }
 
