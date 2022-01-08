88d39f35517802d4e7a165ee2ebb86cdfe0af924
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-08 16:26:04 +0900

    updated

--- typical90/034-ThereAreFewTypesOfElements/there-are-few.cpp
+++ typical90/034-ThereAreFewTypesOfElements/there-are-few.cpp
@@ -41,25 +41,27 @@ int main()
     //  Main
     //  Make Ci
     std::vector< int > C( N );
+
     //  A counter of the number and the appearance time 
+    std::vector< bool > visited( N, false );
     std::map< int, int > counter;
     int head = 0, tail = 0;
-    while( head < N ) {
-        //  Updarte the counter for the tail
-        while( ( tail < N )  && ( counter.size() <= K ) ) {
-            counter[ A.at( tail ) ] ++;
-            tail ++;
-        }
-        while( tail < N ) {
-            bool before_flag = ( counter.size() == K );
-            counter[ A.at( tail ) ] ++;
-            bool after_flag = ( counter.size() == K + 1 );
-
-            if( before_flag && after_flag ) {
+    while( tail < N ) {
+        //  Move the tail
+        while( counter.size() <= K ) {
+            if( !visited.at( tail ) ) {
+                visited.at( tail ) = true;
+                counter[ A.at( tail ) ] ++;
+            }
+            //  The case to break
+            if( counter.size() == K + 1 ) {
                 break;
             }
             else {
                 tail ++;
+                if( tail >= K ) {
+                    break;
+                }
             }
         }
 
@@ -67,24 +69,22 @@ int main()
         C.at( head ) = tail - 1;
         //  Update the counter for the head
         counter[ A.at( head ) ] --;
-        if( Debug ) {
-            for( auto c : counter ) {
-                std::cerr << "( " << c.first << ", " << c.second << " ) ";
-            }
-            std::cerr << std::endl;
-        }
         if( counter[ A.at( head ) ] == 0 ) {
             counter.erase( A.at( head ) );
         }
+
+        //  Move the head
         head ++;
     }
-    
+    for( ; head < N; head ++ ) {
+        C.at( head ) = tail - 1;
+    }
+
     if( Debug ) {
         for( int i = 0; i < N; i ++ ) {
             std::cerr << i << ": " << C.at( i ) << std::endl;
         }
     }
-
     //  Finalize
     if( Debug ) {
         std::cerr << "Normally terminated." << std::endl;
