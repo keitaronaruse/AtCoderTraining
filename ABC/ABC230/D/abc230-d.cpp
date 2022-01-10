4033d69557af6b780c0ec6e888eb34b58f993e03
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-10 12:19:47 +0900

    updated

--- ABC/ABC230/D/abc230-d.cpp
+++ ABC/ABC230/D/abc230-d.cpp
@@ -10,6 +10,7 @@
 
 #include <iostream>
 #include <vector>
+#include <utility>
 #include <algorithm>
 
 const bool Debug = true;
@@ -24,26 +25,51 @@ int main()
     }
     //  Read Li and Ri
     std::vector< int > L( N, 0 ), R( N, 0 );
+    std::vector< std::pair< int, int > > LR( N, std::make_pair( 0, 0 ) );
     for( int i = 0; i < N; i ++ ) {
         std::cin >> L.at( i ) >> R.at( i );
+        L.at( i ) --;
+        R.at( i ) --;
         if( Debug ) {
             std::cerr << L.at( i ) << " " << R.at( i ) << std::endl;
         }
-        L.at( i ) --;
-        R.at( i ) --;
+        LR.at( i ) = std::make_pair( L.at( i ), R.at( i ) );
     }
 
-    //  Main
-    std::vector< int > B = R;
-    std::sort( B.begin(), B.end() );
+    //  Preprocess
+    //  Sort LR by L in the ascending order
+    std::sort( LR.begin(), LR.end(), 
+        []( std::pair< int, int >& a, std::pair< int, int >& b ) { return( a.second < b.second ); }
+    );
     if( Debug ) {
         for( int i = 0; i < N; i ++ ) {
-            std::cerr << B.at( i ) + 1 << std::endl;
+            std::cerr << "( " << LR.at( i ).first << ", " << LR.at( i ).second << " )" <<std::endl;
         }
     }
-    int head  = 0;
-    //  Puch at B[head]
-    B.at( head ) + D;
+    //  Main
+    int head = 0;
+    int count = 1;
+    //  Ri
+    int punch = LR.at( head ).second;
+    while( head < N ) {
+        //  Li
+        if( LR.at( head ).first <= punch + D ) {
+            std::cerr << "Head: " << head << std::endl;
+            // Broken walls
+            head ++;
+        }
+        else {
+            std::cerr << "Punch" << std::endl;
+            punch = LR.at( head ).second;
+            count ++;
+        }
+    }
+    //  if LR.at( head ) > punch
+    //  or head == N
+
+    //  Display result
+    std::cout << count << std::endl;
+
     //  Finalize
     if( Debug ) {
         std::cerr << "Normally terminated." << std::endl;
