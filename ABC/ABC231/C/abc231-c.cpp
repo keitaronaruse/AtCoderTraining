8cbe61e69565641b039b59515542dd8524a31e5b
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-10 21:25:46 +0900

    updated

--- ABC/ABC231/C/abc231-c.cpp
+++ ABC/ABC231/C/abc231-c.cpp
@@ -2,7 +2,7 @@
     ABC231 Problem C - Counting 2
         https://atcoder.jp/contests/abc231/tasks/abc231_c
         Author: Keitaro Naruse
-        Date:   2021-12-11
+        Date:   2021-12-11, 2022-01-10
         MIT License
 */
 
@@ -15,34 +15,45 @@
 #include <vector>
 #include <algorithm>
 
+const bool Debug = false;
+
 int main()
 {
     //  Read N and Q
     int N = 0, Q = 0;
     std::cin >> N >> Q;
+    if( Debug ) {
+        std::cerr << N << " " << Q << std::endl;
+    }
     //  Read Ai
     std::vector< int > A( N );
     for( int i = 0; i < N; i++ ) {
         std::cin >> A.at( i );
-        //  Debug
-        // std::cout << A.at( i ) << " ";
     }
-    //  Debug
-    // std::cout << std::endl;
+    if( Debug ) {
+        for( int i = 0; i < N; i ++ ) {
+            std::cerr << A.at( i ) << " ";
+        }
+        std::cout << std::endl;
+    }
 
     //  Main
     //  Sort Ai and find x by binary search
-    sort( A.begin(), A.end() );
+    std::sort( A.begin(), A.end() );
 
     //  Read xj
-    int x = 0;
     for( int j = 0; j < Q; j ++ ) {
+        int x = 0;
         std::cin >> x;
-        auto it = std::lower_bound( A.begin(), A.end(), x );
+        if( Debug ) {
+            std::cerr << x << std::endl;
+        }
+        std::vector< int >::iterator it = std::lower_bound( A.begin(), A.end(), x );
         std::cout << A.end() - it << std::endl;
     }
 
-    //  Debug
-    // std::cerr << "Normally terminated." << std::endl;
+    if( Debug ) {
+        std::cerr << "Normally terminated." << std::endl;
+    }
     return( 0 );
 }
---
+++ ABC/ABC231/C/make-test-cases.cpp
@@ -0,0 +1,83 @@
+/*
+    make-test-cases.cpp
+    ABC231 Problem C - Counting 2
+        https://atcoder.jp/contests/abc231/tasks/abc231_c
+        Author: Keitaro Naruse
+        Date:   2021-12-11, 2022-01-10
+        MIT License
+*/
+
+#include <iostream>
+#include <random>
+#include <algorithm>
+
+//  Debug flag
+const bool Debug = false;
+
+int main( int argc, char* argv[] )
+{
+    if( argc != 2 ) {
+        std::cerr << "make-test-cases [1-3]" << std::endl;
+        return( -1 );
+    }
+
+    //  Minimum case
+    if( argv[1][0] == '1' ) {
+        const int N = 1;
+        const int Q = 1;
+        std::cout << N << " " << Q << std::endl;
+        for( int i = 0; i < N; i ++ ) {
+            const int A = 1;
+            std::cout << A << " ";
+        }
+        std::cout << std::endl;
+        for( int j = 0; j < Q; j ++ ) {
+            const int x = 1;
+            std::cout << x << std::endl;
+        }
+    }
+    //  Maximum case
+    else if( argv[1][0] == '2' ) {
+        const int N = 200000;
+        const int Q = 200000;
+        std::cout << N << " " << Q << std::endl;
+        for( int i = 0; i < N; i ++ ) {
+            const int A = 1000000000;
+            std::cout << A << " ";
+        }
+        std::cout << std::endl;
+        for( int j = 0; j < Q; j ++ ) {
+            const int x = 1000000000;
+            std::cout << x << std::endl;
+        }
+    }
+    //  Random case
+    else if( argv[1][0] == '3' ) {
+        std::random_device seed_gen;
+        std::default_random_engine engine( seed_gen() );
+        std::uniform_int_distribution< int > dist; 
+
+        dist = std::uniform_int_distribution< int >( 1, 200000 );
+        const int N = dist( engine );
+        const int Q = dist( engine );
+        std::cout << N << " " << Q << std::endl;
+
+        dist = std::uniform_int_distribution< int >( 1, 1000000000 );
+        for( int i = 0; i < N; i ++ ) {
+            const int A = dist( engine );
+            std::cout << A << " ";
+        }
+        std::cout << std::endl;
+
+        for( int j = 0; j < Q; j ++ ) {
+            const int x = dist( engine );
+            std::cout << x << std::endl;
+        }
+    }
+
+    //  Finalize
+    if( Debug ) {
+        std::cerr << "Normally terminated." << std::endl;
+    }
+    return( 0 );
+}
