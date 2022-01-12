4911a22dfe6917656c3498f64aa60360179b09d8
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-12 18:36:59 +0900

    updated

--- nrs/adj-graph.cpp
+++ nrs/adj-graph.cpp
@@ -2,7 +2,7 @@
     nrs::adj_graph
         The class of a graph structure by adjacent nodes 
         Author: Keitaro Naruse
-        Date:   2021-12-31, 2022-01-06
+        Date:   2021-12-31, 2022-01-12
         MIT License
 */
 
@@ -28,13 +28,25 @@ namespace nrs {
             adj_graph( const adj_graph& r ) : n( r.n ) {
                 adj_nodes = r.adj_nodes;
             }
-            void add_edge( int b, int e ) {
+            void add_d_edge( int b, int e ) {
                 adj_nodes.at( b ).push_back( e );
             }
-            bool has_edge( int b, int e ) {
+            void add_u_edge( int b, int e ) {
+                adj_nodes.at( b ).push_back( e );
+                adj_nodes.at( e ).push_back( b );
+            }
+            bool has_d_edge( int b, int e ) {
                 return( 
                     std::find( adj_nodes.at( b ).begin(), adj_nodes.at( b ).end(), e ) 
                     != adj_nodes.at( b ).end()
+                );
+            }
+            bool has_u_edge( int b, int e ) {
+                return( 
+                    ( std::find( adj_nodes.at( b ).begin(), adj_nodes.at( b ).end(), e ) 
+                    != adj_nodes.at( b ).end() ) &&
+                    ( std::find( adj_nodes.at( e ).begin(), adj_nodes.at( e ).end(), b ) 
+                    != adj_nodes.at( e ).end() )
                 );
             }
             /*
@@ -67,10 +79,6 @@ namespace nrs {
                     //  Common procedure
                     for( auto u : adj_nodes.at( v ) ) {
                         if( length.at( u ) == -1 ) {
-                            length.at( u ) = length.at( v ) + 1;
-                            s.push( u );
-                        }
-                        else if( length.at( u ) > length.at( v ) + 1 ) {
                             length.at( u ) = length.at( v ) + 1;
                             s.push( u );
                         }
@@ -114,10 +122,6 @@ namespace nrs {
                             length.at( u ) = length.at( v ) + 1;
                             s.push( u );
                         }
-                        else if( length.at( u ) > length.at( v ) + 1 ) {
-                            length.at( u ) = length.at( v ) + 1;
-                            s.push( u );
-                        }
                     }
                 }
                 if( e == -1 ) {
@@ -158,63 +162,110 @@ namespace nrs {
                         length.at( u ) = length.at( v ) + 1;
                         s.push( u );
                     }
-                    else if( length.at( u ) > length.at( v ) + 1 ) {
-                        length.at( u ) = length.at( v ) + 1;
-                        s.push( u );
-                    }
                 }
                 return( v );
             }
     };
 }
 
+int hw2id( int h, int w, int W )
+{
+    return( h * W + w );
+}
+
+void id2hw( int id, int& h, int& w, int W )
+{
+    w = id % W;
+    h = ( id - w )  / W;
+}
+
+const bool Debug = true;
+
 //  Test driver
 int main()
 {
-    const int N = 4;
-    nrs::adj_graph g( N );
-
-    //  Make a graph
-    std::cerr << "Add the edge ( 0, 1 )" << std::endl;
-    g.add_edge( 0, 1 ); g.add_edge( 1, 0 );
-    std::cerr << "Add the edge ( 1, 2 )" << std::endl;
-    g.add_edge( 1, 2 ); g.add_edge( 2, 1 );
-    std::cerr << "Add the edge ( 0, 3 )" << std::endl;
-    g.add_edge( 0, 3 ); g.add_edge( 3, 0 );
-    std::cerr << "Add the edge ( 2, 3 )" << std::endl;
-    g.add_edge( 2, 3 ); g.add_edge( 3, 2 );
-
-    //  DFS
-    //  DFS: Setup
-    std::vector< int > length( N, -1 );
-    std::stack< int > s;
-    //  DFS: Initial node
-    int b = 0; 
-    length.at( b ) = 0;
-    s.push( b );
-    //  DFS: Main
-    std::cerr << "DFS: ";
-    while( !s.empty() ) {
-        int v = g.next_node_dfs( s, length );
-        std::cerr << v << " ";
-    }
-    std::cerr << std::endl;
+    //  An example of a graph
+    //   012345
+    //  0.#..#.
+    //  1......
+    //  2.#..#.
+    const int H = 3;
+    const int W = 6;
+    const int N = hw2id( H - 1, W - 1, W ) + 1;
     
+    //  Make a graph instance
+    nrs::adj_graph g( N );
+    g.add_u_edge( hw2id( 0, 0, W ), hw2id( 1, 0, W ) ); 
+    g.add_u_edge( hw2id( 1, 0, W ), hw2id( 2, 0, W ) ); 
+    g.add_u_edge( hw2id( 1, 0, W ), hw2id( 1, 1, W ) ); 
+    g.add_u_edge( hw2id( 1, 1, W ), hw2id( 1, 2, W ) );
+    g.add_u_edge( hw2id( 1, 2, W ), hw2id( 0, 2, W ) ); 
+    g.add_u_edge( hw2id( 1, 2, W ), hw2id( 2, 2, W ) );
+    g.add_u_edge( hw2id( 0, 2, W ), hw2id( 0, 3, W ) ); 
+    g.add_u_edge( hw2id( 1, 2, W ), hw2id( 1, 3, W ) );
+    g.add_u_edge( hw2id( 2, 2, W ), hw2id( 2, 3, W ) ); 
+    g.add_u_edge( hw2id( 0, 3, W ), hw2id( 1, 3, W ) ); 
+    g.add_u_edge( hw2id( 2, 3, W ), hw2id( 1, 3, W ) ); 
+    g.add_u_edge( hw2id( 1, 3, W ), hw2id( 1, 4, W ) ); 
+    g.add_u_edge( hw2id( 1, 4, W ), hw2id( 1, 5, W ) ); 
+    g.add_u_edge( hw2id( 1, 5, W ), hw2id( 0, 5, W ) ); 
+    g.add_u_edge( hw2id( 1, 5, W ), hw2id( 2, 5, W ) ); 
+    
+    //  Test edges
+    if( Debug ) {
+        for( int i = 0; i < N; i ++ ) {
+            std::cerr << i << ": ";
+            for( int v : g.adj_nodes.at( i ) ) {
+                std::cerr << v << " ";
+            }
+            std::cerr << std::endl;
+        }
+    }
+    //  Make graph utilities
+    std::vector< int > length;
+    int b = 0, e = 0;
+
     //  BFS
     //  BFS: Setup
     length = std::vector< int > ( N, -1 );
     std::queue< int > q;
     //  BFS: Initial node
-    b = 0; 
+    b = hw2id( 0, 0, W ); e = hw2id( 2, 5, W ); 
     length.at( b ) = 0;
     q.push( b );
     //  BFS: Main
     std::cerr << "BFS: ";
     while( !q.empty() ) {
         int v = g.next_node_bfs( q, length );
-        std::cerr << v << " ";
+        id2hw( v, b, e, W );
+        std::cerr << "( " << b << ", " << e << " )" << " ";
     }
     std::cerr << std::endl;
+    for( auto d : length ) {
+        std::cerr << d << " ";
+    }
+    std::cerr << std::endl;
+
+//  0 -1  4  5 -1  7
+//  1  2  3  4  5  6
+//  2 -1  4  5 -1  7 
+
+    //  DFS
+    //  DFS: Setup
+    // std::vector< int > length( N, -1 );
+    // std::stack< int > s;
+    //  DFS: Initial node
+    // int b = 0; 
+    // length.at( b ) = 0;
+    // s.push( b );
+    //  DFS: Main
+    // std::cerr << "DFS: ";
+    // while( !s.empty() ) {
+    //     int v = g.next_node_dfs( s, length );
+    //     std::cerr << v << " ";
+    // }
+    // std::cerr << std::endl;
+    
     
     //  Variables
     // int length = 0, b = 0, e = 0;
@@ -250,7 +301,6 @@ int main()
     // std::cerr << "BFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
     // b = 2; e = -1; length = g.length_bfs( b, e );
     // std::cerr << "BFS: Farest node from " << b << " is " << e << " with a lenght of  " << length << std::endl; 
-
 
     return( 0 );
 }
