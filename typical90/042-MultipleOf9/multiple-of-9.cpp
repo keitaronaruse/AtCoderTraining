65d982b0c99af04de541fed73e41f1e106e1e11b
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-12 14:24:07 +0900

    updated

--- typical90/042-MultipleOf9/multiple-of-9.cpp
+++ typical90/042-MultipleOf9/multiple-of-9.cpp
@@ -2,35 +2,26 @@
     042 - Multiple of 9（★4）
         https://atcoder.jp/contests/typical90/tasks/typical90_ap
         Author: Keitaro Naruse
-        Date:   2022-01-11
+        Date:   2022-01-11, 2022-01-12
         MIT License
 */
 
 // # Solution
 // - K = (d1,d2,...), d1+d2+...= 9k
-// - K == 9: 
-//   - (1,8), (2,7), (3,6), (4,5) 
-//   - (1,1,7), (1,2,6), (1,3,5), (1,4,4), (2,2,5), (2,3,4), (3,3,3)
-//   - (1,1,1,6), (1,1,2,5), (1,1,3,4), (1,2,2,4), (1,2,3,3),
-//   - (1,1,1,1,5), (1,1,1,2,4), (1,1,1,3,3)
-//   - (1,1,1,1,1,4), (1,1,1,1,2,3), 
-//   - (1,1,1,1,1,1,3), (1,1,1,1,1,2,2)
-//   - (1,1,1,1,1,1,1,2)
-//   - (1,1,1,1,1,1,1,1,1)
-// - K = 18: (9,9)
-//   - 1: 1
-//   - 2: 1+1
+// # dp(k)
+// - dp[0] 
+//   - 1: 1+(0)
+//   - 2: 1+(1)
 //   - 3: 1+(2) = 1+(1+1) 
 //   - 4: 1+(3) = 1+(1+(2))
 //   - k: 1+(k-1) 
-
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-const long long Large_Prime = 1000000007LL;
-const bool Debug = true;
+const int Large_Prime = 1000000007;
+const bool Debug = false;
 
 int main()
 {
@@ -42,28 +33,29 @@ int main()
     }
 
     //  Main
-    std::vector< long long > dp( K + 1, 0LL );
-    dp.at( 0 ) = 1;
-    for( int k = 1; k < K + 1; k ++ ) {
-        const int R = 9;
-        int J = std::min( k + 1, R );
-        for( int j = 1; j < J; j ++ ) {
-            dp.at( k ) += dp.at( k - j );
-            dp.at( k ) %= Large_Prime;
+    const int R = 9;
+    int result = 0LL;
+    if( K % R == 0 ) {
+        std::vector< int > dp( K + 1, 0 );
+        dp.at( 0 ) = 1;
+        for( int k = 1; k < K + 1; k ++ ) {
+            int J = std::min( k + 1, R + 1 );
+            for( int j = 1; j < J; j ++ ) {
+                dp.at( k ) += dp.at( k - j );
+                if( dp.at( k ) > Large_Prime ) {
+                    dp.at( k ) -= Large_Prime;
+                }
+            }
         }
-    }
-    if( Debug ) {
-        for( int k = 0; k < K + 1; k ++ ) {
-            std::cerr << dp.at( k ) << " ";
+        if( Debug ) {
+            for( int k = 0; k < K + 1; k ++ ) {
+                std::cerr << dp.at( k ) << " ";
+            }
+            std::cerr << std::endl;
         }
-        std::cerr << std::endl;
-    }
-    //  Display result
-    long long result = 0LL;
-
-    if( K % 9 == 0 ) {
         result = dp.at( K );
     }
+    //  Display result
     std::cout << result << std::endl;
 
     //  Finalize
