eba15fbe8599e7ef28539cb08de9e88d3be12bb1
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-14 09:51:51 +0900

    updated

--- nrs/maze-2d.cpp
+++ nrs/maze-2d.cpp
@@ -2,7 +2,7 @@
 * @file maze-2d.cpp
 * @brief maze 2d
 * @author Keitaro Naruse
-* @date 2021-01-13
+* @date 2021-01-13, 2021-01-14
 * @copyright MIT License
 */
 
@@ -11,7 +11,29 @@
 #include <vector>
 
 namespace nrs {
-    ;
+    class maze_2d {
+        public:
+            typedef std::vector< std::string > value_type; 
+            int h, w;
+            value_type s;
+        public:
+            maze_2d() : h( 0 ), w( 0 ), s( value_type ( h, std::string("") ) ) {
+            }
+            maze_2d( int H, int W ) : h( H ), w( W ), s( value_type ( h, std::string( w, '.' ) ) ) {
+            }
+            maze_2d( const value_type& S ) : h( S.size() ), w( S.at( 0 ).size() ), s( S ) {
+            }
+            friend std::ostream& operator<< ( std::ostream& os, const maze_2d& m ) {
+                os << m.h << " " << m.w << std::endl;
+                for( int h = 0; h < m.h; h ++ ) {
+                    for( int w = 0; w < m.s.at( h ).size(); w ++ ) {
+                        os << m.s.at( h ).at( w );
+                    }
+                    os << std::endl;
+                }
+            }
+
+    };
 }
 
 const bool Debug = true;
@@ -24,15 +46,19 @@ int main()
         ".....#.#",
         "#..#...."
     };
-    const int H = S.size();
-    const int W = S.at( 0 ).size();
+
+    nrs::maze_2d m1;
+    nrs::maze_2d m2( 4, 8 );
+    nrs::maze_2d m3 = m2;
+    nrs::maze_2d m4( S );
 
     if( Debug ) {
-        std::cerr << H << " " << W << std::endl;
-        for( int i = 0; i < S.size(); i ++ ) {
-            std::cerr << S.at( i ) << std::endl;
-        }
+        std::cerr << m1;
+        std::cerr << m2;
+        std::cerr << m3;
+        std::cerr << m4;
     }
+
     if( Debug ) {
         std::cerr << "Normally terminated." << std::endl;
     }
