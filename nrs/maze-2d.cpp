808848c3103a2275f56c9f7b1b51cf88f0f0b21e
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-14 20:23:21 +0900

    updated

--- nrs/maze-2d.cpp
+++ nrs/maze-2d.cpp
@@ -12,14 +12,9 @@
 #include <queue>
 #include <stack>
 #include <utility>
+#include <functional>
 
 namespace nrs {
-    struct comp {
-        bool operator ()(const T& x, const T& y) const {
-            return( x > y );
-        };
-    };
-
     class maze_2d {
         typedef std::pair< int, int > value_type;
         public:
@@ -28,7 +23,11 @@ namespace nrs {
             std::vector< std::vector< int > > length;
             std::queue< value_type > q;
             std::stack< value_type > s;
-            std::priority_queue< value_type > pq;
+            std::priority_queue< 
+                std::pair< int, value_type >, 
+                std::vector< std::pair< int, value_type > >,
+                std::greater< std::pair< int, value_type > >
+            > pq;
         public:
             maze_2d() : height( 0 ), width( 0 ), 
                 maze( std::vector< std::string >( height, std::string( "" ) ) ),
@@ -41,9 +40,6 @@ namespace nrs {
             maze_2d( const std::vector< std::string >& S ) : height( S.size() ), width( S.at( 0 ).size() ),
                 maze( S ), 
                 length( height, std::vector< int >( width, -1 ) ) {
-            }
-            void init_length() {
-                length = std::vector< std::vector< int > >( height, std::vector< int >( width, -1 ) );
             }
             std::ostream& print_maze( std::ostream& os ) {
                 for( int i = 0; i < height; i ++ ) {
@@ -102,7 +98,35 @@ namespace nrs {
                         if( maze.at( i ).at( j ) == '.' ) {
                             if( length.at( i ).at( j ) == -1 ) {
                                 length.at( i ).at( j ) = length.at( h ).at( w ) + 1;
-                                s.push( std::make_pair( i, j) );
+                                s.push( std::make_pair( i, j ) );
+                            }
+                        }
+                    }
+                }
+                return( v );
+            }
+
+            std::pair< int, std::pair< int, int > > next_node_4_priority_queue() {
+                std::pair< int, value_type > v = pq.top(); 
+                int d = v.first;
+                int h = v.second.first, w = v.second.second; 
+                pq.pop();
+                for( int k = 0; k < 4; k ++ ) {
+                    int i = 0, j = 0;
+                    switch( k ) {
+                        case 0: i = h + 1; j = w + 0; break;
+                        case 1: i = h + 0; j = w + 1; break;
+                        case 2: i = h - 1; j = w + 0; break;
+                        case 3: i = h + 0; j = w - 1; break;
+                    }
+                    if( 0 <= i && i < height && 0 <= j && j < width ) {
+                        if( maze.at( i ).at( j ) == '.' ) {
+                            if( length.at( i ).at( j ) == -1 ) {
+                                length.at( i ).at( j ) = length.at( h ).at( w ) + 1;
+                                pq.push( std::make_pair( 
+                                        length.at( i ).at( j ), std::make_pair( i, j ) 
+                                    ) 
+                                );
                             }
                         }
                     }
@@ -132,7 +156,7 @@ int main()
     int h = 0, w = 0;
     m.length.at( h ).at( w ) = 0;
 
-    const int mode = 0;
+    const int mode = 2;
     if( mode == 0 ) {
         //  BFS: Breadth first search, queue 
         m.q.push( std::make_pair( h, w ) );
@@ -182,17 +206,33 @@ int main()
     //  8  7  6  5  6 -1  8 -1 
     // -1  8  7 -1 11 10  9 10 
     else if( mode == 2 ) {
-        // auto comp = [ &length ]( std::pair< int, int >& a, std::pair< int, int >& b ) {
-        //     return( length[]);
-        // }
-        // //  Dijkstra's algorithm: priority queue 
-        // std::priority_queue< 
-        //     std::pair< int, int >, 
-        //     std::vector< std::pair< int, int > >, 
-        //     decltype()
-        // > pq();
-        
+        //  Dijkstra's algorithm: priority queue 
+        m.pq.push( std::make_pair(
+                0, 
+                std::make_pair( h, w ) 
+            )
+        ); 
+        while( !m.pq.empty() ) {
+            std::pair< int, std::pair< int, int > > v = m.next_node_4_priority_queue( );
+            if( Debug ) {
+                std::cerr << "( " << v.second.first << ", " << v.second.second << " )" << " ";
+            }
+        }
+        if( Debug ) {
+            std::cerr << std::endl;
+        }
     }
+    // Dijkstra's method: the order of node visit
+    // ( 0, 0 ) ( 0, 1 ) ( 1, 0 ) ( 0, 2 ) ( 2, 0 ) 
+    // ( 0, 3 ) ( 2, 1 ) ( 1, 3 ) ( 2, 2 ) ( 3, 1 ) 
+    // ( 1, 4 ) ( 2, 3 ) ( 3, 2 ) ( 1, 5 ) ( 2, 4 ) 
+    // ( 0, 5 ) ( 1, 6 ) ( 3, 4 ) ( 1, 7 ) ( 2, 6 ) 
+    // ( 3, 5 ) ( 0, 7 ) ( 3, 6 ) ( 3, 7 ) 
+    // Dijkstra's algorithm: length  
+    //  0  1  2  3 -1  7 -1  9 
+    //  1 -1 -1  4  5  6  7  8 
+    //  2  3  4  5  6 -1  8 -1 
+    // -1  4  5 -1  7  8  9 10 
     if( Debug ) {
         m.print_length( std::cerr );
     }
