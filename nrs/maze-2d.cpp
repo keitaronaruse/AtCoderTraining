d6a109d1741653757305ddd1177e649c3b0280ba
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-17 18:20:39 +0900

    updated

--- nrs/maze-2d.cpp
+++ nrs/maze-2d.cpp
@@ -30,8 +30,7 @@ namespace nrs {
                 std::pair< int, state_type >, 
                 std::vector< std::pair< int, state_type > >,
                 std::greater< std::pair< int, state_type > >
-            > pq;
-            std::deque< state_type > dq;
+            > visiting_priority_queue;
         public:
             maze_2d() : height( 0 ), width( 0 ), 
                 maze( std::vector< std::string >( height, std::string( "" ) ) ),
@@ -111,32 +110,24 @@ namespace nrs {
                 return( h_w );
             }
 
-            std::pair< int, std::pair< int, int > > next_node_4_priority_queue() {
-                std::pair< int, state_type > v = pq.top(); 
-                int d = v.first;
-                int h = v.second.first, w = v.second.second; 
-                pq.pop();
-                for( int k = 0; k < 4; k ++ ) {
-                    int i = 0, j = 0;
-                    switch( k ) {
-                        case 0: i = h + 1; j = w + 0; break;
-                        case 1: i = h + 0; j = w + 1; break;
-                        case 2: i = h - 1; j = w + 0; break;
-                        case 3: i = h + 0; j = w - 1; break;
-                    }
-                    if( 0 <= i && i < height && 0 <= j && j < width ) {
-                        if( maze.at( i ).at( j ) == '.' ) {
-                            if( length.at( i ).at( j ) == -1 ) {
-                                length.at( i ).at( j ) = length.at( h ).at( w ) + 1;
-                                pq.push( std::make_pair( 
-                                        length.at( i ).at( j ), std::make_pair( i, j ) 
-                                    ) 
-                                );
-                            }
-                        }
+            std::pair< int, state_type > next_node_four_neighbor_dijkstra() {
+                std::pair< int, state_type > l_h_w = visiting_priority_queue.top(); 
+                visiting_priority_queue.pop();
+                state_type h_w = l_h_w.second;
+                int h = h_w.first, w = h_w.second;
+                std::vector< state_type > neighbors = get_four_neighbors( h_w );
+                for( auto n : neighbors ) {
+                    int i = n.first, j = n.second;
+                    if( length.at( i ).at( j ) == -1 ) {
+                        length.at( i ).at( j ) = length.at( h ).at( w ) + 1;
+                        visiting_priority_queue.push( std::make_pair( 
+                                length.at( i ).at( j ), std::make_pair( i, j ) 
+                            ) 
+                        );
+
                     }
                 }
-                return( v );
+                return( l_h_w );
             }
     };
 }
@@ -161,7 +152,7 @@ int main()
     int h = 0, w = 0;
     m.length.at( h ).at( w ) = 0;
 
-    const int mode = 1;
+    const int mode = 2;
     if( mode == 0 ) {
         //  BFS: Breadth first search, queue 
         m.visiting_queue.push( std::make_pair( h, w ) );
@@ -212,15 +203,15 @@ int main()
     // -1  8  7 -1 11 10  9 10 
     else if( mode == 2 ) {
         //  Dijkstra's algorithm: priority queue 
-        m.pq.push( std::make_pair(
+        m.visiting_priority_queue.push( std::make_pair(
                 0, 
                 std::make_pair( h, w ) 
             )
         ); 
-        while( !m.pq.empty() ) {
-            std::pair< int, std::pair< int, int > > v = m.next_node_4_priority_queue( );
+        while( !m.visiting_priority_queue.empty() ) {
+            std::pair< int, nrs::maze_2d::state_type > l_h_w = m.next_node_four_neighbor_dijkstra( );
             if( Debug ) {
-                std::cerr << "( " << v.second.first << ", " << v.second.second << " )" << " ";
+                std::cerr << "( " << l_h_w.second.first << ", " << l_h_w.second.second << " )" << " ";
             }
         }
         if( Debug ) {
