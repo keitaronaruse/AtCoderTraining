90409ab10dd15c415259d62bc15a85c190ee9f5c
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-17 23:39:29 +0900

    updated

--- typical90/043-MazeChallengeWithLackOfSleep/maze-challenge.cpp
+++ typical90/043-MazeChallengeWithLackOfSleep/maze-challenge.cpp
@@ -15,34 +15,37 @@
 #include <utility>
 #include <deque>
 
+struct state {
+    int h; // [ 0, H )
+    int w; // [ 0, W )
+    int q; // 0(Right: w ++), 1(Up: h --), 2(Left: w --), 3 (Down: h++) 
+};
+
 const bool Debug = true;
 
 int main()
 {
+    //  Constants
+    const int Q = 4;
     //  Read H and W
-    int H = 0, W = 0;
-    std::cin >> H >> W;
+    int H = 0, W = 0; std::cin >> H >> W;
     if( Debug ) {
         std::cerr << H << " " << W << std::endl;
     }
     //  Read Rs and Cs
-    int Rs = 0, Cs = 0;
-    std::cin >> Rs >> Cs;
+    int Rs = 0, Cs = 0; std::cin >> Rs >> Cs;
     if( Debug ) {
         std::cerr << Rs << " " << Cs << std::endl;
     }
-    Rs --; 
-    Cs --;
+    Rs --; Cs --;
     //  Read Rt and Ct
-    int Rt = 0, Ct = 0;
-    std::cin >> Rt >> Ct;
+    int Rt = 0, Ct = 0; std::cin >> Rt >> Ct;
     if( Debug ) {
         std::cerr << Rt << " " << Ct << std::endl;
     }
-    Rt --;
-    Ct --;
+    Rt --; Ct --;
     //  Read S
-    std::vector< std::vector< char > > S( H, std::vector< char >( W, ' ') );
+    std::vector< std::vector< char > > S( H, std::vector< char >( W, ' ' ) );
     for(int h = 0; h < H; h ++ ) {
         for( int w = 0; w < W; w ++ ) {
             std::cin >> S.at( h ).at( w );
@@ -58,34 +61,24 @@ int main()
     }
 
     //  Main
-    std::deque< std::pair< int, int > > dq;
-    std::vector< std::vector< int > > length( H, std::vector< int >( W, -1 ) );
-    int h = Rs, w = Cs;
-    dq.push_back( std::make_pair( h, w ) );
-    length.at( h ).at( w ) = 0;
-    while( !dq.empty() ) {
-        auto p = dq.front();
-        h = p.first;
-        w = p.second;
-        std::cerr << h << " " << w << std::endl;
-        dq.pop_front();
-        int i = 0, j = 0;
-        for( int k = 0; k < 4; k ++ ) {
-            switch( k ) {
-                case 0: i =  1; j =  0; break;
-                case 1: i =  0; j = -1; break;
-                case 2: i = -1; j =  0; break;
-                case 3: i =  0; j =  1; break;
-            }
-            if( 0 <= h + i && h + i < H && 0 <= w + j && w + j < W ) {
-                if( S.at( h + i ).at( w + j ) == '.' ) {
-                    if( length.at( h + i ).at( w + j ) == -1 ) {
-                        length.at( h + i ).at( w + j ) = length.at( h ).at( w ) + 1;
-                        dq.push_back( std::make_pair( h + i, w + j ) );
-                    }
-                }
-            }
+    std::deque< state > deq;
+    std::vector< std::vector< std::vector< int > > > length( 
+        H, std::vector< std::vector< int> > ( 
+            W, std::vector< int > ( Q, -1 ) 
+        ) 
+    );
+    //  Initial value
+    int h = Rs, w = Cs, q = 0;
+    for( q = 0; q < Q; q ++ ) {
+        length.at( h ).at( w ).at( q ) = 0;
+        deq.push_back( { h, w, q } );
+    }
+    while( !deq.empty() ) {
+        struct state s = deq.front();
+        if( Debug ) {
+            std::cerr << s.h << " " << s.w << " " << s.q << std::endl;
         }
+        deq.pop_front();
     }
 
     //  Finalize
