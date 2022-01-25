7b04dd0c788ea61e47fed3783b4e2773245dd8a5
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-25 19:48:44 +0900

    updated

--- ABC/ABC236/D/abc236-d.cpp
+++ ABC/ABC236/D/abc236-d.cpp
@@ -1,6 +1,6 @@
 /**
 * @file abc236-d.cpp
-* @brief ABC236 Problem D
+* @brief ABC236 Problem D - Dance
 * @author Keitaro Naruse
 * @date 2022-01-23
 * @copyright MIT License
@@ -16,18 +16,45 @@
 
 const bool Debug = true;
 
-bool next_combination()
+//  N = 2, H = 1    [ 0 ]
+//  N = 4, H = 2    [ 0 ][ 1-3 ]   
+//  N = 6, H = 3    [ 0 ][ 1-4 ][ 2-5 ]   
+//  N = 8, H = 4    [ 0 ][ 1-5 ][ 2-6 ][ 3-7 ]
+//  N = 16, H = 8   [ 0 ][ 1-9 ][ 2-10 ][ 3-11 ][ 4-12 ][ 5-13 ][ 6-14 ][ 7-15 ]
+
+class division {
+    public:
+        int n, h;
+        std::vector< int > first, second;
+        std::vector< int > min_value, max_value;
+    public:
+        division( int n_ ) : n( n_ ), h( n_ / 2 ) {
+            first  = std::vector< int >( h, 0 );
+            second = std::vector< int >( h, 0 );
+            min_value = std::vector< int >( h, 0 );
+            max_value = std::vector< int >( h, 0 );
+            min_value.at( 0 ) = 0;
+            max_value.at( 0 ) = 0;
+            for( int i = 1; i < h; i ++ ) {
+                min_value.at( i ) = i;
+                max_value.at( i ) = i + h;
+            }
+            for( int i = 0; i < h; i ++ ) {
+                first.at( i ) = min_value.at( i );
+            }
+        }
+        bool next()
+        {
+            return( true );
+        }
+};
+
+std::ostream& operator<<( std::ostream& os, const std::vector< int >& v )
 {
-    //  2N=2,   0/1, x1/0
-    //  2N=4,   01/23, 02/13, 03/12, 
-    //          x12/03, x13/02, x23/01
-    //  2n=4,   01/23, 02/13, 03/12
-    //  2N=6,   012/345, 013/245, 014/235, 015/234, 023/145, 
-    //          024/135, 025/134, 034/125, 035/124, 045/123,
-    //          x123/045, x124/035, x125/034, x134/025, x135/024
-    //          x145/023, x234/015, x235/014, x245/013, x345/012
-    //  2N=8,   0123/4567 
-    return( true );
+    for( auto k : v ) {
+        os << k << " ";
+    }
+    return( os );
 }
 
 int main()
@@ -38,41 +65,29 @@ int main()
     if( Debug ) {
         std::cerr << N << std::endl;
     }
-    //  Read Aij = [ 0, 2^30 ) 
-    std::vector< std::vector< int > > A( 2 * N, std::vector< int>( 2 * N, -1 ) );
-    for( int i = 0; i < 2 * N - 1; i ++ ) {
-        for( int j = i + 1; j < 2 * N; j ++ ) {
-            std::cin >> A.at( i ).at( j );
-            A.at( j ).at( i ) = A.at( i ).at( j );
-        }
-    }
+
+    division d( N );
     if( Debug ) {
-        for( int i = 0; i < 2 * N; i ++ ) {
-            for( int j = 0; j < 2 * N; j ++ ) {
-                std::cerr << A.at( i ).at( j ) << " ";
-            }
-            std::cerr << std::endl;
-        }
-    }
-    std::vector< int > index;
-    for( int i = 0; i < 2 * N; i ++) {
-        index.push_back( i );
-    }
-    for( auto i : index ) {
-        std::cerr << i << " ";
-    }
-    std::cerr << std::endl;
-    for( int i = 0; i < 2 * N - 1; i ++ ) {
-        for( int j = i + 1; j < 2 * N; j ++) {
-            std::swap( index.at( i ), index.at( j ) );
-            for( auto i : index ) {
-                std::cerr << i << " ";
-            }
-            std::cerr << std::endl;
-            std::swap( index.at( i ), index.at( j ) );
-        }
+        std::cerr << "First:  " << d.first << std::endl;   
     }
 
+
+    // //  Read Aij = [ 0, 2^30 ) 
+    // std::vector< std::vector< int > > A( 2 * N, std::vector< int>( 2 * N, -1 ) );
+    // for( int i = 0; i < 2 * N - 1; i ++ ) {
+    //     for( int j = i + 1; j < 2 * N; j ++ ) {
+    //         std::cin >> A.at( i ).at( j );
+    //         A.at( j ).at( i ) = A.at( i ).at( j );
+    //     }
+    // }
+    // if( Debug ) {
+    //     for( int i = 0; i < 2 * N; i ++ ) {
+    //         for( int j = 0; j < 2 * N; j ++ ) {
+    //             std::cerr << A.at( i ).at( j ) << " ";
+    //         }
+    //         std::cerr << std::endl;
+    //     }
+    // }
 
     //  Finalize
     if( Debug ) {
