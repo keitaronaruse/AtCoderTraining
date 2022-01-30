97b6dd27115a8df568a3bae85cf5358a4a17d9b4
keitaronaruse <naruse@u-aizu.ac.jp>
2022-01-30 12:46:56 +0900

    updated

--- ARC/ARC134/B/arc134-b.cpp
+++ ARC/ARC134/B/arc134-b.cpp
@@ -13,8 +13,26 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <map>
 
-const bool Debug = true;
+std::ostream& operator<<( std::ostream& os, const std::map< char, int >& m )
+{
+    for( auto p : m ) {
+        std::cerr << "( " << p.first << ": " << p.second << " )" << " ";
+    }
+    return( os );
+}
+
+const bool Debug = false;
+
+char find_smallest_char( std::map< char, int >& m )
+{
+    char ch = 'a';
+    while( m[ ch ] == 0 ) {
+        ch ++;
+    }
+    return( ch );
+}
 
 int main()
 {
@@ -25,26 +43,61 @@ int main()
         std::cerr << N << std::endl;
     }
     //  Read S
-    std::string s;
-    std::cin >> s;
+    std::string S;
+    std::cin >> S;
     if( Debug ) {
-        std::cerr << s << std::endl;
+        std::cerr << S << std::endl;
     }
 
     //  Main
-    //  Analysis
-    
-    //  check no need to swap
-    bool is_no_change = true;
-    for( int i = 0; i < N - 1; i ++ ) {
-        if( s.at( i ) > s.at( i + 1)  ) {
-            is_no_change = false;
+    std::string s = S;
+    //  Analysis: character -> frequency
+    std::map< char, int > counters;
+    for( int i = 0; i < s.size(); i ++ ) {
+        counters[ s.at(i) ] ++;
+    }
+    if( Debug ) {
+        std::cerr << counters << std::endl;
+    }
+    //  Main
+    int l = 0, r = N - 1;
+    while( l < r ) {
+        //  Find the target character
+        char ch = find_smallest_char( counters );
+        if( Debug ) {
+            std::cerr << ch << " ";
+        }
+        //  Find r, the tail end
+        for( ; r > l; r -- ) {
+            if( s.at( r ) == ch ) {
+                break;
+            }
+        }
+        //  Find l, the head end
+        for( ; l < r; l ++ ) {
+            if( s.at( l ) > s.at( r ) ) {
+                break;
+            }
+        }
+        if( Debug ) {
+            std::cerr << l << " " << r << " " << std::endl;
+        }
+        //  Swap the characters at l and r
+        if( l < r ) {
+            std::swap( s.at( l ), s.at( r ) );
+            //  Update the counter
+            counters[ s.at( l ) ]--;
+            counters[ s.at( r ) ]--;
+        }
+        else {
             break;
         }
+        if( Debug ) {
+            std::cerr << counters << std::endl;
+        }
     }
-    if( is_no_change ) {
-        std::cout << s << std::endl;
-    }
+    //  Display the result
+    std::cout << s << std::endl;
     //  Finalize
     if( Debug ) {
         std::cerr << "Normally terminated." << std::endl;
