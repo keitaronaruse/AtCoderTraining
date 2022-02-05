5e9b87d8c678d4252f3a01cfa7bcb89d9e1a61d4
keitaronaruse <naruse@u-aizu.ac.jp>
2022-02-05 11:58:31 +0900

    updated

--- educational_dp/I-Coins/I-Coins.cpp
+++ educational_dp/I-Coins/I-Coins.cpp
@@ -8,10 +8,11 @@
 */
 
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <algorithm>
 
-const bool Debug = true;
+const bool Debug = false;
 
 int main()
 {
@@ -35,9 +36,28 @@ int main()
 
     //  Main
     std::vector< std::vector< double > > dp_prob( N + 1, std::vector< double >( N + 1, 0.0 ) );
-
+    dp_prob.at( 0 ).at( 0 ) = 1.0;
+    for( int i = 0; i < N; i ++ ) {
+        for( int j = 0; j < N; j ++ ) {
+            dp_prob.at( i + 1 ).at( j + 1 ) += dp_prob.at( i ).at( j ) * p.at( i );
+            dp_prob.at( i + 1 ).at( j ) += dp_prob.at( i ).at( j ) * ( 1.0 - p.at( i ) );
+        }
+    }
+    if( Debug ) {
+        for( int i = 0; i <= N; i ++ ) {
+            for( int j = 0; j <= N; j ++ ) {
+                std::cerr << dp_prob.at( i ).at( j ) << " ";
+            }
+            std::cerr << std::endl;
+        }
+    }
+    //  Find the solution
+    double prob = 0.0;
+    for( int j = N / 2 + 1; j <= N; j ++ ) {
+        prob += dp_prob.at( N ).at( j );
+    }
     //  Display the result
-    std::cout << dp_prob.at( N ).at( N ) << std::endl;
+    std::cout << std::fixed << std::setprecision(10) << prob << std::endl;
 
     //  Finalize
     if( Debug ) {
