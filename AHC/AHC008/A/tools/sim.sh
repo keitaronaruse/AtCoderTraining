11a63e597fdac25ef3f089ef3bed65d6b0a7ed22
keitaronaruse <naruse@u-aizu.ac.jp>
2022-02-26 10:40:56 +0900

    updated

--- AHC/AHC008/A/tools/sim.sh
+++ AHC/AHC008/A/tools/sim.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cargo run --release --bin tester ../build/ahc008-a-8 < "in/$1" > "out/$1" 2> "pets/$1"
+cargo run --release --bin tester ../build/ahc008-a-7 < "in/$1" > "out/$1" 2> "pets/$1"
