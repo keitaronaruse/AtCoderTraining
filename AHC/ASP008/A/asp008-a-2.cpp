e5df181798bd1e9662c053298c13769b5606cc31
keitaronaruse <naruse@u-aizu.ac.jp>
2022-03-09 16:35:14 +0900

    updated

--- AHC/ASP008/A/asp008-a-2.cpp
+++ AHC/ASP008/A/asp008-a-2.cpp
@@ -16,7 +16,7 @@
 #include <algorithm>
 #include <utility>
 
-const bool Debug = true;
+const bool Debug = false;
 
 //  S = [ 4, 10 ]: The number of Shapes 
 //  C = [ 4, 10 ]: The number of Colors
@@ -68,6 +68,36 @@ void read_input()
 
 }
 
+void print_input( std::ostream& os )
+{
+    os << S << " " << C << " " << H << " " << a << " " << b << std::endl;
+    for( int s = 0; s < S; s ++ ) {
+        for( int c = 0; c < C; c ++ ) {
+            os << N.at( s ).at( c ) << " ";
+        }
+        os << std::endl;
+    }
+
+    for( int s = 0; s < S; s ++ ) {
+        os << K.at( s ) << " ";
+    }
+    os << std::endl;
+
+    for( int i = 0; i < S; i ++ ) {
+        for( int j = 0; j < S; j ++ ) {
+            os << A.at( i ).at( j ) << " ";
+        }
+        os << std::endl;
+    }
+
+    for( int i = 0; i < C; i ++ ) {
+        for( int j = 0; j < C; j ++ ) {
+            os << B.at( i ).at( j ) << " ";
+        }
+        os << std::endl;
+    }
+}
+
 void write_output( const std::vector< std::pair< int, int > > &ans ) {
     std::cout << ans.size() << std::endl;
     for( int i = 0; i < int(ans.size()); i++ ) {
@@ -78,6 +108,51 @@ void write_output( const std::vector< std::pair< int, int > > &ans ) {
             std::cout << ans[i].first << std::endl;
         }
     }
+}
+
+int calc_X( std::vector< std::pair< int, int > >& ans )
+{
+    std::vector< int > t( H, -1 );
+    std::vector< int > r = K;
+    int X = 0;
+    int h = 0;
+    for( size_t i = 0; i < ans.size(); i ++ ) {
+        int s = ans.at( i ).first;
+        int c = ans.at( i ).second;
+        if( s == -1 && t.at( h ) != -1 ) {
+            r.at( t.at( h ) ) ++;
+            X ++;
+            t.at( h ) = 0;
+        }
+        else if( 0 <= s && s < S && t.at( h ) == -1 ) {
+            r.at( s ) --;
+            X ++;
+            t.at( h ) = s;
+        }
+        else if( 0 <= s && s < S && t.at( h ) != -1 && t.at( h ) != s ) {
+            r.at( t.at( h ) ) ++;
+            r.at( s ) --;
+            X += 2;
+            t.at( h ) = s;
+        }
+        h ++;
+        h %= H;
+    }
+
+    return( X );
+}
+
+int calc_Y( std::vector< std::pair< int, int > >& ans )
+{
+    int L = ( int ) ans.size();
+    int Y = L;
+    for( int s = 0; s < S; s ++ ) {
+        for( int c = 0; c < C; c ++ ) {
+            Y -= N.at( s ).at( c );
+        }
+    }
+
+    return( Y );
 }
 
 void main_rtn( std::vector< std::pair< int, int > >& ans )
@@ -125,51 +200,108 @@ void main_rtn( std::vector< std::pair< int, int > >& ans )
     }
 }
 
+std::vector< std::pair< int, int > > make_plan( const std::vector< std::pair< int, int > >& s_c_order )
+{
+    std::vector< std::pair< int, int > > plan;
+
+    std::vector< int > hanger_on_hook( H, -1 );
+    std::vector< int > hangers_available = K;
+    int pos = 0, prev_s = -1, prev_c = -1;
+
+    for( auto p : s_c_order ) {
+        int s = p.first;
+        int c = p.second;
+
+        if( N.at( s ).at( c ) == 0 ) {
+            continue;
+        }
+
+        // need this amount of empty hooks
+        const int setup = (prev_s == -1 ? 0 : std::max( A.at( prev_s ).at( s ), B.at( prev_c ).at( c )) );
+        for( int i = 0; i < setup; i ++ ) {
+            plan.push_back( std::make_pair( -2, 0 ) );
+            (++ pos) %= H;
+        }
+
+        // put (s, c)
+        for( int n = 0; n < N.at( s ).at( c ); ) {
+            if( hanger_on_hook.at( pos ) != s && hangers_available.at( s ) == 0 ) {
+                // run out of hangers
+                plan.push_back( std::make_pair( -2, 0 ) );
+                (++ pos) %= H;
+            }
+            else {
+                plan.push_back( std::make_pair( s, c ) );
+                n ++;
+                if( hanger_on_hook.at( pos ) != s ) {
+                    if( hanger_on_hook.at( pos ) >= 0 ) {
+                        hangers_available.at( hanger_on_hook.at( pos ) ) ++;
+                    }
+                    hangers_available.at( s ) --;
+                    hanger_on_hook.at( pos ) = s;
+                }
+                (++ pos) %= H;
+            }
+            prev_s = s;
+            prev_c = c;
+        }
+    }
+
+    return( plan );
+}
+
+std::vector< std::pair< int, int > > make_default_order()
+{
+    std::vector< std::pair< int, int > > order;
+    for( int s = 0; s < S; s ++ ) {
+        for( int c = 0; c < C; c ++ ) {
+            order.push_back( std::make_pair( s, c ) );
+        }
+    }
+    return( order );
+}
+
+std::vector< std::pair< int, int > > make_minimum_setup_order()
+{
+    std::vector< std::pair< int, int > > order;
+
+    int n = S * C;
+    for( int s = 0; s < S; s ++ ) {
+        for( int c = 0; c < C; c ++ ) {
+            int index = s * C + c; 
+            order.push_back( std::make_pair( s, c ) );
+        }
+    }
+    return( order );
+}
+
 int main()
 {
     //  Read input
     read_input();
     if( Debug ) {
-        std::cerr << S << " " << C << " " << H << " " << a << " " << b << std::endl;
-        for( int s = 0; s < S; s ++ ) {
-            for( int c = 0; c < C; c ++ ) {
-                std::cerr << N.at( s ).at( c ) << " ";
-            }
-            std::cerr << std::endl;
-        }
-
-        for( int s = 0; s < S; s ++ ) {
-            std::cerr << K.at( s ) << " ";
-        }
-        std::cerr << std::endl;
-
-        for( int i = 0; i < S; i ++ ) {
-            for( int j = 0; j < S; j ++ ) {
-                std::cerr << A.at( i ).at( j ) << " ";
-            }
-            std::cerr << std::endl;
-        }
-
-        for( int i = 0; i < C; i ++ ) {
-            for( int j = 0; j < C; j ++ ) {
-                std::cerr << B.at( i ).at( j ) << " ";
-            }
-            std::cerr << std::endl;
-        }
+        print_input( std::cerr );
     }
 
     //  Main
-    //  Same as sample.cpp
-    std::vector< std::pair< int, int > > ans;
-    main_rtn( ans );
+    std::vector< std::pair< int, int > > order = make_default_order();
+    std::vector< std::pair< int, int > > plan = make_plan( order );
+
+    //  Calculate the score
+    int X = calc_X( plan );
+    int Y = calc_Y( plan );
+    int score = std::max( 1, 10000000 - a * X - b * Y );
+    if( Debug ) {
+        std::cerr << X << " " << Y << " " << score << std::endl;
+    }
 
     // write answer
-    write_output( ans );
-
+    write_output( plan );
 
     //  Finalize
     if( Debug ) {
         std::cerr << "Normally terminated." << std::endl;
-    }    
+    }
+
     return( 0 );
 }
