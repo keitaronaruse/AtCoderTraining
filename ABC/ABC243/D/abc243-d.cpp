2c19a9435502220a8a2d24a9b7789199900d1fa0
keitaronaruse <naruse@u-aizu.ac.jp>
2022-03-13 10:21:06 +0900

    updated

--- ABC/ABC243/D/abc243-d.cpp
+++ ABC/ABC243/D/abc243-d.cpp
@@ -1,8 +1,8 @@
 /**
 * @file abc243-d.cpp
-* @brief ABC243 Problem Moves on Binary TreD
+* @brief ABC243 Problem D - Moves on Binary Tree
 * @author Keitaro Naruse
-* @date 2022-03-12
+* @date 2022-03-12, 2022-03-13
 * @copyright MIT License
 * @details https://atcoder.jp/contests/abc243/tasks/abc243_d
 */
@@ -11,63 +11,47 @@
 
 #include <iostream>
 #include <string>
+#include <deque>
 
 int main()
 {
-    //  Read N = [ 1, 10^6 ]
+    //  Read N = [ 1, 10^6 ], X = [ 1, 10^18 ]
     int N;
     long long X;
     std::cin >> N >> X;
-    //  Read | S | = [ 1, 10^5 ]
+    //  Read | S | = [ 1, 10^6 ]
     std::string S;
     std::cin >> S;
 
     //  Main
-    std::string s, t = S;
-    bool odd = (X % 2L == 1L);
-    do {
-        s = t;
-        t = "";
-        int i = 0;
-        for( int i = 0; i < s.size(); ) {
-            if( i < s.size() - 1 && s.at( i ) == 'R' && s.at( i + 1 ) == 'U' ) {
-                i += 2;
-            }
-            else if(  i < s.size() - 1 && s.at( i ) == 'L' && s.at( i + 1 ) == 'U' ) {
-                i += 2;
-            }
-            else if( i < s.size() - 1 && odd && s.at( i ) == 'U' && s.at( i + 1 ) == 'R' ) {
-                i += 2;
-            }
-            else if( i < s.size() - 1 && !odd && s.at( i ) == 'U' && s.at( i + 1 ) == 'L' ) {
-                i += 2;
+    std::deque< char > s_stack;
+    for( int i = 0; i < N; i ++ ) {
+        if( s_stack.empty() ) {
+            s_stack.push_back( S.at( i ) );
+        }
+        else {
+            char c = s_stack.back();
+            if( ( c == 'R' || c == 'L' ) && S.at( i ) == 'U' ) {
+                //  (R, U) and (L, U) are canceled
+                s_stack.pop_back();
             }
             else {
-                t.push_back( s.at( i ) );
-                i ++;
+                s_stack.push_back( S.at( i ) );
             }
         }
-    } while( s != t );
-
-    if( s.size() == 0 ) {
-        std::cout << X << std::endl;
     }
-    else {
-        long long x = X;
-        for( int i = 0; i < s.size(); i ++ ) {
-            if( s.at( i ) == 'U' ) {
-                x = x / 2L;
-            }
-            else if( s.at( i ) == 'L' ) {
-                x = x * 2L;
-            }
-            else if( s.at( i ) == 'R' ) {
-                x = x * 2L + 1L;
-            }
+
+    long long x = X;
+    for( int i = 0; i < ( int ) s_stack.size(); i ++ ) {
+        switch( s_stack.at( i ) ) {
+            case 'U': x = x / 2L; break;
+            case 'L': x = x * 2L; break;
+            case 'R': x = x * 2L + 1L; break;
+            default: break;
         }
-        std::cout << x << std::endl;
     }
-
+    std::cout << x << std::endl;
+    
     //  Finalize
     return( 0 );
 }
