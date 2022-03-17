b06e7b057b4dee6ef7e972477bcfdb28b74ddf4b
keitaronaruse <naruse@u-aizu.ac.jp>
2022-03-18 08:50:12 +0900

    updated

--- PAST/PAST09/J/past09-j.cpp
+++ PAST/PAST09/J/past09-j.cpp
@@ -20,34 +20,39 @@ std::ostream& operator<<( std::ostream& os, const std::vector< T >& v )
     return( os );
 }
 
-void paint_point( std::vector< std::vector< int > >& board, int x, int y, int r, int ud, int lr, int N ) 
+void paint_point( std::vector< std::vector< int > >& board, int x, int y, int r, int ud, int lr ) 
 {
+    int N = board.size();
     int h, w;
     if( r == 0 ) {
-        h = x; w = y;
+        h = x; 
+        w = y;
     }
     else if( r == 1 ) {
-        h = x; w = N - 1 - y; 
+        h = N - 1 - y; 
+        w = x;
     }
     else if( r == 2 ) {
-        h = N - x; w = N - 1 - y; 
+        h = N - 1 - x; 
+        w = N - 1 - y; 
     }
     else if( r == 3 ) {
-        h = N - 1 - x; w = y;
+        h = y; 
+        w = N - 1 - x; 
     }
 
     if( ud == 1 ) {
         h = N - 1 - h;
     }
 
-    if( lr == 1) {
-        w = N - w;
+    if( lr == 1 ) {
+        w = N - 1 - w;
     }
 
     board.at( h ).at( w ) = ( board.at( h ).at( w ) + 1 ) % 2;
 }
 
-std::vector< std::vector< int > > rotate_board( const std::vector< std::vector< int > >& board, int r )
+std::vector< std::vector< int > > rotate_board( const std::vector< std::vector< int > >& board, int r, int ud, int lr )
 {
     int N = board.size();
     std::vector< std::vector< int > > screen = board;
@@ -55,19 +60,30 @@ std::vector< std::vector< int > > rotate_board( const std::vector< std::vector< 
         for( int y = 0; y < N; y ++ ) {
             int h, w;
             if( r == 0 ) {
-                h = x; w = y;
+                h = x; 
+                w = y;
             }
             else if( r == 1 ) {
-                h = x; w = N - 1 - y; 
+                h = y; 
+                w = N - 1 - x; 
             }
             else if( r == 2 ) {
-                h = N - 1 - x; w = N - 1 - y; 
+                h = N - 1 - x; 
+                w = N - 1 - y; 
             }
             else if( r == 3 ) {
-                h = N - 1 - x; w = y;
+                h = N - 1 - y; 
+                w = x;
+            }
+
+            if( ud == 1 ) {
+                h = N - 1 - h;
+            }
+            if( lr == 1 ) {
+                w = N - 1 - w;
             }
             screen.at( h ).at( w ) = board.at( x ).at( y ) ;
-        };
+        }
     }
     return( screen );
 }
@@ -83,7 +99,7 @@ int main()
     //  Main
     std::vector< std::vector< int > > board( N, std::vector< int > ( N, 0 ) );
     //  Modes
-    //  r == 0 if 0 deg., 1 == 90 deg., 2 == 180 deg., 3 == 270 deg.
+    //  r == 0 if nothing, 1 if A or BBB, 2 if AA or BB, 3 if AAA or B
     int r = 0;
     //  ud == 0 if normal, 1 == up-down flipped
     int ud = 0;
@@ -95,16 +111,16 @@ int main()
         if( q == 1 ) {
             int x, y;
             std::cin >> x >> y;
-            paint_point( board, x - 1, y - 1, r, ud, lr, N );
+            paint_point( board, x - 1, y - 1, r, ud, lr );
         }
         else if( q == 2 ) {
             char c;
             std::cin >> c;
             if( c == 'A' ) {
-                r --;
+                r ++;
             }
             else if( c == 'B' ) {
-                r ++;
+                r --;
             }
             r = ( r + 4 ) % 4;
         }
@@ -120,22 +136,15 @@ int main()
         }
     }
 
-    for( int i = 0; i < N; i ++ ) {
-        for( int j = 0; j < N; j ++ ) {
-            std::cout << board.at( i ).at( j );
-        }
-        std::cout << std::endl;
-    }
-
     //  Output
-    board = rotate_board( board, r );
-
+    board = rotate_board( board, r, ud, lr );
     for( int i = 0; i < N; i ++ ) {
         for( int j = 0; j < N; j ++ ) {
             std::cout << board.at( i ).at( j );
         }
         std::cout << std::endl;
     }
+    std::cout << r << " " << ud << " " << lr << std::endl;
 
     return( 0 );
 }
