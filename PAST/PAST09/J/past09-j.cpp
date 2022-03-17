bf4c1e7bda4bd370b7d23e0feddbfb80168f8018
keitaronaruse <naruse@u-aizu.ac.jp>
2022-03-17 13:52:21 +0900

    updated

---
+++ PAST/PAST09/J/past09-j.cpp
@@ -0,0 +1,141 @@
+/**
+* @file past09-j.cpp
+* @brief PAST 9 Problem J - 回転と反転
+  @author Keitaro Naruse
+* @date 2022-03-17
+* @copyright MIT License
+* @details https://atcoder.jp/contests/past202112-open/tasks/past202112_j
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template < class T >
+std::ostream& operator<<( std::ostream& os, const std::vector< T >& v )
+{
+    for( const auto& e : v ) {
+        os << e << " ";
+    }
+    return( os );
+}
+
+void paint_point( std::vector< std::vector< int > >& board, int x, int y, int r, int ud, int lr, int N ) 
+{
+    int h, w;
+    if( r == 0 ) {
+        h = x; w = y;
+    }
+    else if( r == 1 ) {
+        h = x; w = N - 1 - y; 
+    }
+    else if( r == 2 ) {
+        h = N - x; w = N - 1 - y; 
+    }
+    else if( r == 3 ) {
+        h = N - 1 - x; w = y;
+    }
+
+    if( ud == 1 ) {
+        h = N - 1 - h;
+    }
+
+    if( lr == 1) {
+        w = N - w;
+    }
+
+    board.at( h ).at( w ) = ( board.at( h ).at( w ) + 1 ) % 2;
+}
+
+std::vector< std::vector< int > > rotate_board( const std::vector< std::vector< int > >& board, int r )
+{
+    int N = board.size();
+    std::vector< std::vector< int > > screen = board;
+    for( int x = 0; x < N; x ++ ) {
+        for( int y = 0; y < N; y ++ ) {
+            int h, w;
+            if( r == 0 ) {
+                h = x; w = y;
+            }
+            else if( r == 1 ) {
+                h = x; w = N - 1 - y; 
+            }
+            else if( r == 2 ) {
+                h = N - 1 - x; w = N - 1 - y; 
+            }
+            else if( r == 3 ) {
+                h = N - 1 - x; w = y;
+            }
+            screen.at( h ).at( w ) = board.at( x ).at( y ) ;
+        };
+    }
+    return( screen );
+}
+
+int main()
+{
+    //  N = [ 1, 300 ]
+    int N;
+    //  Q = [ 1, 2 * 10^5 ]
+    int Q;
+    std::cin >> N >> Q;
+
+    //  Main
+    std::vector< std::vector< int > > board( N, std::vector< int > ( N, 0 ) );
+    //  Modes
+    //  r == 0 if 0 deg., 1 == 90 deg., 2 == 180 deg., 3 == 270 deg.
+    int r = 0;
+    //  ud == 0 if normal, 1 == up-down flipped
+    int ud = 0;
+    //  lr == 0 if normal, 1 == left-right flipped
+    int lr = 0;
+    for( int i = 0; i < Q; i ++ ) {
+        int q;
+        std::cin >> q;
+        if( q == 1 ) {
+            int x, y;
+            std::cin >> x >> y;
+            paint_point( board, x - 1, y - 1, r, ud, lr, N );
+        }
+        else if( q == 2 ) {
+            char c;
+            std::cin >> c;
+            if( c == 'A' ) {
+                r --;
+            }
+            else if( c == 'B' ) {
+                r ++;
+            }
+            r = ( r + 4 ) % 4;
+        }
+        else if( q == 3 ) {
+            char c;
+            std::cin >> c;
+            if( c == 'A' ) {
+                ud = ( ud + 1 ) % 2; 
+            }
+            else if( c == 'B' ) {
+                lr = ( lr + 1 ) % 2; 
+            }
+        }
+    }
+
+    for( int i = 0; i < N; i ++ ) {
+        for( int j = 0; j < N; j ++ ) {
+            std::cout << board.at( i ).at( j );
+        }
+        std::cout << std::endl;
+    }
+
+    //  Output
+    board = rotate_board( board, r );
+
+    for( int i = 0; i < N; i ++ ) {
+        for( int j = 0; j < N; j ++ ) {
+            std::cout << board.at( i ).at( j );
+        }
+        std::cout << std::endl;
+    }
+
+    return( 0 );
+}
---
+++ nrs/graph.cpp
@@ -0,0 +1,202 @@
+/**
+* @file graph.cpp
+* @brief nrs::graph< Weight >
+  @author Keitaro Naruse
+* @date 2022-03-17
+* @copyright MIT License
+* @details 
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <queue>
+#include <utility>
+
+namespace nrs {
+    template < class Weight >
+    class graph {
+        public:
+            typedef int Node;
+            typedef std::pair< Weight, int > Weight_Node;
+            typedef std::pair< int, Weight > Node_Weight;
+        private:
+            int n;
+            Weight inf;
+            //  { v, w } = weight.at( u ).at( k )
+            std::vector< std::vector< Node_Weight > > weight;
+            //  A path length from a certain node, l = length.at( u )
+            std::vector< Weight > length;
+            //  A previous node used in the dijkstra algorithm, v = prev_node.at( u )
+            std::vector< Node > prev_node;
+            //  A proproty queue for the dijkstra algorithm p_que = ( w, u )
+            std::priority_queue< 
+                Weight_Node, 
+                std::vector< Weight_Node >,
+                std::greater< Weight_Node >
+            > p_que;
+        public:
+            graph() : n( 0 ) {}
+            graph( int N, Weight INF ) : n( N ), inf( INF ) {
+                init_weights();
+                init_lengths();
+                init_prev_nodes();
+            }
+            graph( const graph< Weight >& r ) {
+                weight = r.weight;
+                length = r.length;
+                prev_node = r.prev_node;
+            }
+            void init_weights() {
+                weight = std::vector< std::vector< Node_Weight > >( n );
+            }
+            void init_lengths() {
+                length = std::vector< Weight > ( n, inf );
+            }
+            void init_prev_nodes() {
+                prev_node = std::vector< Node > ( n, -1 );
+            }
+            void add_edge( Node u, Node v, Weight w = Weight( 1 ) ) {
+                weight.at( u ).push_back( std::make_pair( v, w ) );
+                weight.at( v ).push_back( std::make_pair( u, w ) );
+            }
+            bool has_edge( Node u, Node v ) {
+                for( const auto& p : weight.at( u ) ) {
+                    if( p.first == v ) {
+                        for( const auto& q : weight.at( v ) ) {
+                            if( q.first == u ) {
+                                return( true );
+                            }
+                        }
+                    }
+                }
+                return( false );
+            }
+            void delete_edge( Node u, Node v ) {
+                for( auto it = weight.at( u ).begin(); it < weight.at( u ).end(); it ++  ) {
+                    if( *it.first == v ) {
+                        weight.at( u ).erase( it );
+                    }
+                }
+                for( auto it = weight.at( v ).begin(); it < weight.at( v ).end(); it ++  ) {
+                    if( *it.first == u ) {
+                        weight.at( v ).erase( it );
+                    }
+                }
+            }
+            std::ostream& print_edges( std::ostream& os ) const {
+                for( int u = 0; u < weight.size(); u ++ ) {
+                    for( auto q : weight.at( u ) ) {
+                        int v = q.first;
+                        Weight w = q.second;
+                        os << "( " << u << ", " << v << ", " << w << " ) ";
+                    }
+                }
+                os << std::endl;
+                return( os );
+            }
+            Weight find_shortest_path_dijkstra( Node b, Node e ) {
+                init_lengths();
+                init_prev_nodes();
+
+                length.at( b ) = Weight( 0 );
+                prev_node.at( b ) = Node( -1 );
+                p_que.push( std::make_pair( length.at( b ), b ) );
+
+                while( !p_que.empty() ) {
+                    auto p = p_que.top(); 
+                    Weight d = p.first;
+                    Node u = p.second;
+                    p_que.pop();
+
+                    //  When a node ( d, u ) in the priority que should be expanded
+                    if( length.at( u ) >= d ) {
+                        //  Add a node to priority que
+                        for( auto q : weight.at( u ) ) {
+                            Node v = q.first;
+                            Weight c = q.second;
+                            if( length.at( v ) > d + c ) {
+                                length.at( v ) = d + c;
+                                prev_node.at( v ) = u;
+                                p_que.push( std::make_pair( length.at( v ), v ) );
+                            }
+                        }
+                    }
+                }
+                return( length.at( e ) ); 
+            }
+            std::vector< Node > back_trace( Node b, Node e ) {
+                std::vector< Node > path;
+                Node v = e;
+                while( v != b ) {
+                    path.push_back( v );
+                    v = prev_node.at( v );
+                }
+                path.push_back( b );
+                std::reverse( path.begin(), path.end() );
+                return( path );
+            }
+    };
+}
+
+template < class T >
+std::ostream& operator<<( std::ostream& os, const std::vector< T >& v )
+{
+    for( const auto& e : v ) {
+        os << e << " ";
+    }
+    return( os );
+}
+
+int main()
+{
+    //  Constant
+    const long long INF = 1000000000000000001L;
+
+    //  N = [ 2, 10^18 ]
+    long long N;
+    //  M = [ 1, 10^5 ]
+    int M;
+    std::cin >> N >> M;
+
+    //  A, B = [ 1, N ], C = [ 1, 10^18 ]
+    std::vector< long long > A( M ), B( M ), C( M ); 
+    for( int i = 0; i < M; i ++ ) {
+        std::cin >> A.at( i ) >> B.at( i ) >> C.at( i );
+    }
+
+    //  Main
+    //  Preprocess of nodes
+    std::vector< long long > nodes;
+    nodes.reserve( 2 * M + 2 );
+    nodes.push_back( 1L );
+    nodes.push_back( N );
+    for( int i = 0; i < M; i ++ ) {
+        nodes.push_back( A.at( i ) );
+        nodes.push_back( B.at( i ) );
+    }
+    std::sort( nodes.begin(), nodes.end() );
+    nodes.erase( std::unique( nodes.begin(), nodes.end() ), nodes.end() );
+
+    //  Make a graph instance
+    const int V = ( int ) nodes.size();
+    nrs::graph< long long > g( V, INF );
+    for( int i = 0; i < V - 1; i ++ ) {
+        long long w = nodes.at( i + 1 ) - nodes.at( i );
+        g.add_edge( i, i + 1, w );
+    }
+    for( int i = 0; i < M; i ++ ) {
+        int u = std::lower_bound( nodes.begin(), nodes.end(), A.at( i ) ) - nodes.begin();
+        int v = std::lower_bound( nodes.begin(), nodes.end(), B.at( i ) ) - nodes.begin();
+        g.add_edge( u, v, C.at( i ) );
+    }
+    std::cout << g.find_shortest_path_dijkstra( 0, V - 1 ) << std::endl;
+    // for( auto k : g.back_trace( 0, V - 1 ) ) {
+    //     std::cout << nodes.at( k ) << " ";
+    // }
+    // std::cout << std::endl;
+
+    return( 0 );
+}
