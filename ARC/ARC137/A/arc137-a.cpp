90921b1143f38ae620c7b4a47ec7da606650a9b1
keitaronaruse <naruse@u-aizu.ac.jp>
2022-03-18 21:08:34 +0900

    updated

---
+++ ARC/ARC137/A/arc137-a.cpp
@@ -0,0 +1,46 @@
+/**
+* @file arc137-a.cpp
+* @brief ABC137 Problem A
+* @author Keitaro Naruse
+* @date 2022-03-19
+* @copyright MIT License
+* @details https://atcoder.jp/contests/arc137/tasks/arc137_a
+*/
+
+// # Solution
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+template< class T >
+std::ostream& operator<<( std::ostream& os, const std::vector< T >& v )
+{
+    for( auto k : v ) {
+        os << k << " ";
+    }
+    return( os );
+}
+
+int main()
+{
+    //  Read N = [ 1, 10^3 ]
+    int N = 0;
+    std::cin >> N;
+
+    //  Read Ai = [ 0, 10^9 ]
+    std::vector< int > A( N, 0 );
+    for( int i = 0; i < N; i ++ ) {
+        std::cin >> A.at( i );
+    }
+
+    //  Read | S | = [ 1, 10^6 ]
+    std::string S = "";
+    std::cin >> S;
+
+    //  Main
+
+    //  Finalize
+    return( 0 );
+}
