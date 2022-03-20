1083ac1a33b7034138f3195dc046154d70fd55a0
keitaronaruse <naruse@u-aizu.ac.jp>
2022-03-20 20:09:42 +0900

    updated

---
+++ ABC/ABC244/B/abc244-b.cpp
@@ -0,0 +1,46 @@
+/**
+* @file abc244-b.cpp
+* @brief ABC244 Problem B
+* @author Keitaro Naruse
+* @date 2022-03-20
+* @copyright MIT License
+* @details https://atcoder.jp/contests/abc244/tasks/abc244_b
+*/
+
+// # Solution
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+template< class T >
+std::ostream& operator<<( std::ostream& os, const std::vector< T >& v )
+{
+    for( auto k : v ) {
+        os << k << " ";
+    }
+    return( os );
+}
+
+int main()
+{
+    //  Read N = [ 1, 10^3 ]
+    int N = 0;
+    std::cin >> N;
+
+    //  Read Ai = [ 0, 10^9 ]
+    std::vector< int > A( N, 0 );
+    for( int i = 0; i < N; i ++ ) {
+        std::cin >> A.at( i );
+    }
+
+    //  Read | S | = [ 1, 10^6 ]
+    std::string S = "";
+    std::cin >> S;
+
+    //  Main
+
+    //  Finalize
+    return( 0 );
+}
