8a50829c37d4bb627b5bdbddff87093037f16ca4
keitaronaruse <naruse@u-aizu.ac.jp>
2022-03-24 13:18:26 +0900

    updated

---
+++ AHC/AHC009/A/ahc009-a-1.cpp
@@ -0,0 +1,46 @@
+/**
+* @file ahc009-a.cpp
+* @brief AHC009 Problem A
+* @author Keitaro Naruse
+* @date 2022-03-26
+* @copyright MIT License
+* @details https://atcoder.jp/contests/ahc009/tasks/ahc009_a
+*/
+
+// # Solution
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+template< class T >
+std::ostream& operator<<( std::ostream& os, const std::vector< T >& v )
+{
+    for( auto k : v ) {
+        os << k << " ";
+    }
+    return( os );
+}
+
+int main()
+{
+    //  Read N = [ 1, 10^3 ]
+    int N = 0;
+    std::cin >> N;
+
+    //  Read Ai = [ 0, 10^9 ]
+    std::vector< int > A( N, 0 );
+    for( int i = 0; i < N; i ++ ) {
+        std::cin >> A.at( i );
+    }
+
+    //  Read | S | = [ 1, 10^6 ]
+    std::string S = "";
+    std::cin >> S;
+
+    //  Main
+
+    //  Finalize
+    return( 0 );
+}
---
+++ PAST/PAST09/M/bbst.cpp
@@ -0,0 +1,109 @@
+/**
+* @file bbst.cpp
+* @brief PAST 9 Problem M - 名前の変更
+  @author Keitaro Naruse
+* @date 2022-03-22
+* @copyright MIT License
+* @details https://atcoder.jp/contests/past202112-open/tasks/past202112_m
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <utility>
+
+namespace nrs {
+    ;
+}
+
+template < class T >
+std::ostream& operator<<( std::ostream& os, const std::vector< T >& v ) 
+{
+    for( auto k : v ) {
+        os << k << " ";
+    }
+    os << std::endl;
+    return( os );
+}
+
+int main()
+{
+    //  Read N, Q = [ 1, 10^5 ]
+    int N, Q;
+    std::cin >> N >> Q;
+
+    //  Read S( N )
+    std::vector< std::string > S( N );
+    for( int i = 0; i < N; i ++ ) {
+        std::cin >> S.at( i );
+    }
+
+    //  Read X( Q ), T( Q )
+    std::vector< int > X( Q );
+    std::vector< std::string > T( Q );
+    for( int j = 0; j < Q; j ++ ) {
+        std::cin >> X.at( j ) >> T.at( j );
+    }
+
+    //  Preprocess
+    //  Master name list M( N + Q )
+    std::vector< std::string > M( N + Q );
+    //  Copy S to M
+    std::copy( S.begin(), S.end(), std::back_inserter( M ) );
+    //  Copy T to M
+    std::copy( T.begin(), T.end(), std::back_inserter( M ) );
+    //  Sort and erase duplicates
+    std::sort( M.begin(), M.end() );
+    M.erase( std::unique( M.begin(), M.end() ), M.end() );
+
+    //  Make a table of S -> id in M
+    std::vector< int > s_id( N );
+    for( int i = 0; i < N; i ++ ) {
+        s_id.at( i ) = std::lower_bound( M.begin(), M.end(), S.at( i ) ) - M.begin();
+    }
+
+    //  Make a table of T -> id in M
+    std::vector< int > t_id( Q );
+    for( int j = 0; j < Q; j ++ ) {
+        t_id.at( j ) = std::lower_bound( M.begin(), M.end(), T.at( j ) ) - M.begin();
+    }
+
+    //  Main
+    using nid_i = std::pair< int, int >;
+    std::set< nid_i > nid_i_s;
+
+    // BBST< nid_i> set;
+    //  Put (Si, i) as ( s_id, i )
+    // for( int i = 0; i < N; i ++ ) {
+    //     // nid_i_s.insert( std::make_pair( s_id.at( i ), i ) );
+    //     set.insert( std::make_pair( s_id.at( i ), i ) );
+    // }
+
+    //  Query
+    // for( int j = 0; j < Q; j ++ ) {
+        // auto it = nid_i_s.begin(); 
+        // for( int i = 0; i < X.at( j ) - 1; i ++ ) {
+        //     it ++;
+        // }
+        // int k = it -> second; 
+        // s_id.at( k ) = t_id.at( j );
+        // nid_i_s.erase( it ); 
+        // nid_i_s.insert( std::make_pair( s_id.at( k ), k ) );
+
+    //     auto itr = set.find_by_order( X.at( j ) - 1 );
+    //     int k = itr -> second;
+    //     s_id.at( k ) = t_id.at( j );
+    //     set.erase( itr );
+    //     set.insert( std::make_pair( s_id.at( k ), k ) );
+    // }
+
+    //  Output
+    for( int i = 0; i < N; i ++ ) {
+        std::cout << M.at( s_id.at( i ) ) << " ";
+    }
+    std::cout << std::endl;
+    
+    return( 0 );
+}
