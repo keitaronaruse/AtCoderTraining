6d6adabf28279cf9f6bb7e931bfe19e32471eb23
keitaronaruse <naruse@u-aizu.ac.jp>
2022-03-28 17:11:41 +0900

    updated

--- ARC/ARC132/B/arc132-b.cpp
+++ ARC/ARC132/B/arc132-b.cpp
@@ -12,6 +12,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <utility>
+#include <queue>
 
 template< class T >
 std::ostream& operator<<( std::ostream& os, const std::vector< T >& v )
@@ -35,28 +37,47 @@ int main()
 
     //  Main
     //  Judge if we should reverse it
+    int pos_one = 0;
     bool is_ascending = true;
     if( p.at( n - 1 ) == 1 ) {
-        if( p.at( 0 ) == 2 ) {
-            is_ascending = true;
-        }
-        else {
-            is_ascending = false;
-        }
+        pos_one = n - 1;
+        is_ascending = ( p.at( 0 ) == 2 ) ? true : false;
     }
     else {
         for( int i = 0; i < n - 1; i ++ ) {
             if( p.at( i ) == 1 ) {
-                if( p.at( i ) == 2 ) {
-                    is_ascending = true;
-                }
-                else {
-                    is_ascending = false;
-                }
+                pos_one = i;
+                is_ascending = ( p.at( i + 1 ) == 2 ) ? true : false;
                 break;
             }
         }
     }
+
+    //  search
+    int num_operation = 0;
+    std::queue< std::pair< int , bool > > q;
+    q.push( std::make_pair( pos_one, is_ascending ) );
+    while( !q.empty() ) {
+        auto p = q.front();
+        int pos_one = p.first;
+        bool is_ascending = p.second; 
+        q.pop();
+        if( pos_one == 0 && is_ascending ) {
+            break;
+        }
+        else {
+            //  Operation 1
+            q.push( std::make_pair( n - 1 - pos_one, !is_ascending ) );
+            num_operation ++;
+
+            //  Operation 2
+            if( pos_one != 0 ) {
+                q.push( std::make_pair( pos_one - 1, is_ascending ) );
+                num_operation ++;
+            }
+        }
+    }
+    std::cout << num_operation << std::endl;
 
     //  Finalize
     return( 0 );
