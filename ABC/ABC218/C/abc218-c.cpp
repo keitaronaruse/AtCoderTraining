2051e9e567815478cce372af2807d1b37d10350f
keitaronaruse <naruse@u-aizu.ac.jp>
2022-04-21 19:05:26 +0900

    updated

--- ABC/ABC218/C/abc218-c.cpp
+++ ABC/ABC218/C/abc218-c.cpp
@@ -36,8 +36,26 @@ bool match( const std::vector< std::string >& T, const std::vector< std::string 
     const int N = T.size();
     const int H = P.size();
     const int W = P.at( 0 ).size();
+    bool is_match = false;
+    for( int v = 0; v <= N - H; v ++ ) {
+        for( int u = 0; u <= N - W; u ++ ) {
+            bool is_same = true;
+            for( int i = 0; is_same && i < H; i ++ ) {
+                for( int j = 0; j < W; j ++ ) {
+                    if( T.at( i + v ).at( j + u ) != P.at( i ).at( j ) ) {
+                        is_same = false;
+                        break;
+                    }
+                }
+            }
+            if( is_same ) {
+                is_match = true;
+                break;
+            }
+        }
+    }
 
-    return( true );
+    return( is_match );
 }
 
 int main()
@@ -56,6 +74,22 @@ int main()
 
     //  Main
     //  Preprocess
+    int num_S = 0, num_T = 0;
+    for( int i = 0; i < N; i ++ ) {
+        for( int j = 0; j < N; j ++ ) {
+            if( S.at( i ).at( j ) == '#' ) {
+                num_S ++;
+            }
+            if( T.at( i ).at( j ) == '#' ) {
+                num_T ++;
+            }
+        }
+    }
+    if( num_S != num_T ) {
+        std::cout << "No" << std::endl;
+        return( 0 );
+    }
+
     std::vector< int > S_row( N, 0 ), S_col( N, 0 );
     for( int i = 0; i < N; i ++ ) {
         for( int j = 0; j < N; j ++ ) {
@@ -65,7 +99,7 @@ int main()
             }
         }
     }
-    int L, R, U, D;
+    int L = 0, R = 0, U = 0, D = 0;
     for( int j = 0; j < N; j ++ ) {
         if( S_col.at( j ) != 0 ) {
             L = j; break;
@@ -91,63 +125,47 @@ int main()
     bool isYes = false;
     for( int k = 0; k < 4; k ++ ) {
         if( k == 0 ) {
-            std::vector< std::string > P( D - U + 1, std::string( '.', R - L + 1 ) );
+            std::vector< std::string > P( D - U + 1, std::string( R - L + 1, '.' ) );
             for( int i = 0; i <= D - U; i ++ ) {
                 for( int j = 0; j <= R - L; j ++ ) {
                     P.at( i ).at( j ) = S.at( i + U ).at( j + L );
                 }
             }
-            for( int v = 0; v < N - ( D - U + 1 ); v ++ ) {
-                for( int u = 0; u < N - ( R - L + 1 ); u ++ ) {
-                    if( isYes = match( T, P ) ) {
-                        break;
-                    }
-                }
+            if( ( isYes = match( T, P ) ) ) {
+                break;
             }
         }
         else if( k == 1 ) {
-            std::vector< std::string > P( R - L + 1, std::string( '.', D - U + 1 ) );
+            std::vector< std::string > P( R - L + 1, std::string( D - U + 1, '.' ) );
             for( int i = 0; i <= D - U; i ++ ) {
                 for( int j = 0; j <= R - L; j ++ ) {
                     P.at( R - L - j ).at( i ) = S.at( i + U ).at( j + L );
                 }
             }
-            for( int v = 0; v < N - ( R - L + 1 ); v ++ ) {
-                for( int u = 0; u < N - ( D - U + 1 ); u ++ ) {
-                    if( isYes = match( T, P ) ) {
-                        break;
-                    }
-                }
+            if( ( isYes = match( T, P ) ) ) {
+                break;
             }
         }
         else if( k == 2 ) {
-            std::vector< std::string > P( D - U + 1, std::string( '.', R - L + 1 ) );
+            std::vector< std::string > P( D - U + 1, std::string( R - L + 1, '.' ) );
             for( int i = 0; i <= D - U; i ++ ) {
                 for( int j = 0; j <= R - L; j ++ ) {
                     P.at( D - U - i ).at( R - L - j ) = S.at( i + U ).at( j + L );
                 }
             }
-            for( int v = 0; v < N - ( D - U + 1 ); v ++ ) {
-                for( int u = 0; u < N - ( R - L + 1 ); u ++ ) {
-                    if( isYes = match( T, P ) ) {
-                        break;
-                    }
-                }
+            if( ( isYes = match( T, P ) ) ) {
+                break;
             }
         }
         else {
-            std::vector< std::string > P( R - L + 1, std::string( '.', D - U + 1 ) );
+            std::vector< std::string > P( R - L + 1, std::string( D - U + 1, '.' ) );
             for( int i = 0; i <= D - U; i ++ ) {
                 for( int j = 0; j <= R - L; j ++ ) {
                     P.at( j ).at( D - U - i ) = S.at( i + U ).at( j + L );
                 }
             }
-            for( int v = 0; v < N - ( R - L + 1 ); v ++ ) {
-                for( int u = 0; u < N - ( D - U + 1 ); u ++ ) {
-                    if( isYes = match( T, P ) ) {
-                        break;
-                    }
-                }
+            if( ( isYes = match( T, P ) ) ) {
+                break;
             }
         }
     } 
