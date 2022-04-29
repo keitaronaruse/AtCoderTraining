3b90a25451d6bafa0c1e0e4097f60eb1bf38dd0b
keitaronaruse <naruse@u-aizu.ac.jp>
2022-04-29 20:56:06 +0900

    updated

--- ARC/ARC138/B/arc138-b.cpp
+++ ARC/ARC138/B/arc138-b.cpp
@@ -1,6 +1,6 @@
 /**
 * @file arc138-b.cpp
-* @brief ARC138 Problem X
+* @brief ARC138 Problem B - 01 Generation 
 * @author Keitaro Naruse
 * @date 2022-04-09, 2022-04-28
 * @copyright MIT License
@@ -31,22 +31,56 @@ std::ostream& operator<<( std::ostream& os, const std::vector< T >& v )
 
 int main()
 {
-    //  Read N = [ 1, 10^3 ]
-    int N = 0;
+    //  Read N = [ 1, 2*10^5 ]
+    int N;
     std::cin >> N;
 
-    //  Read Ai = [ 0, 10^9 ]
-    std::vector< int > A( N, 0 );
+    //  Read Ai = [ 0, 1 ]
+    std::vector< int > A( N );
     for( int i = 0; i < N; i ++ ) {
         std::cin >> A.at( i );
     }
 
-    //  Read | S | = [ 1, 10^6 ]
-    std::string S = "";
-    std::cin >> S;
-
     //  Main
+    bool is_flipped = false;
+    int l = 0, r = N - 1;
 
+    int head = A.at( l ), tail = A.at( r );
+    while( l != r ) {
+        if( is_flipped ) {
+            head = A.at( l ) ^ 1;
+            tail = A.at( r ) ^ 1;
+        }
+        else {
+            head = A.at( l );
+            tail = A.at( r );
+        }
+        if( tail == 0 ) {
+            r --;
+        }
+        else if( head == 0 ){
+            l ++ ;
+            is_flipped = is_flipped ? false : true;
+        }
+        else {
+            break;
+        }
+    }
+
+    bool isYes = true;
+    if( l == r ) {
+        isYes = ( head == 0 ) ? true : false;
+    }
+    else {
+        isYes = false;
+    }
+
+    if( isYes ) {
+        std::cout << "Yes" << std::endl;
+    }
+    else {
+        std::cout << "No" << std::endl;
+    }
     //  Finalize
     return( 0 );
 }
