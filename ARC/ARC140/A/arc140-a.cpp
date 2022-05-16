d83fdbc20faafbc7853cb98c44f4f35de9a806e4
keitaronaruse <naruse@u-aizu.ac.jp>
2022-05-16 13:05:31 +0900

    updated

--- ARC/ARC140/A/arc140-a.cpp
+++ ARC/ARC140/A/arc140-a.cpp
@@ -1,6 +1,6 @@
 /**
  * @file arc140-a.cpp
- * @brief ARC140 Problem A
+ * @brief ARC140 Problem A - Right String
  * @author Keitaro Naruse
  * @date 2022-05-15
  * @copyright MIT License
@@ -9,16 +9,10 @@
 
 // # Solution
 
-#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-
-template < class K, class V >
-std::ostream& operator<<( std::ostream& os, const std::pair< K, V >& p ) {
-    os << "( " << p.first << ", " << p.second << " )";
-    return ( os );
-}
+#include <set>
 
 template < class T >
 std::ostream& operator<<( std::ostream& os, const std::vector< T >& v ) {
@@ -37,23 +31,93 @@ std::ostream& operator<<( std::ostream& os,
     return ( os );
 }
 
-int main( ) {
-    //  Read N = [ 1, 10^9 ]
-    int N;
-    std::cin >> N;
-
-    //  Read Ai = [ 0, 10^9 ]
-    std::vector< int > A( N );
-    for( int i = 0; i < N; i++ ) {
-        std::cin >> A.at( i );
+int hamming_dist( const std::string& s, const std::string& t ) {
+    int dist = 0;
+    for( int i = 0; i < s.size( ); i++ ) {
+        if( s.at( i ) != t.at( i ) ) {
+            dist++;
+        }
     }
+    return ( dist );
+}
 
-    //  Read | S | = [ 1, 10^6 ]
+int main( ) {
+    //  Read N = [ 1, 2*10^3 ], K = [ 1, N ]
+    int N, K;
+    std::cin >> N >> K;
+
+    //  Read | S | = [ 1, 2*10^3 ]
     std::string S;
     std::cin >> S;
 
     //  Main
+    std::vector< std::string > dict( N );
+    std::string s = S, t = s;
+    for( int i = 0; i < N; i++ ) {
+        dict.at( i ) = t;
+        t = t.substr( 1, N - 1 ) + t.substr( 0, 1 );
+    }
+
+    std::vector< int > dist( N, 0 );
+    int sum_dist = 0;
+    for( int i = 1; i < N; i++ ) {
+        dist.at( i ) = hamming_dist( dict.at( 0 ), dict.at( i ) );
+        sum_dist += dist.at( i );
+    }
+
     int answer = 0;
+    for( int k = 0; sum_dist != 0 && k < K; k++ ) {
+        int idx_dist2 = -1;
+        for( int i = 1; i < N; i++ ) {
+            if( dist.at( i ) == 2 ) {
+                idx_dist2 = i;
+                break;
+            }
+        }
+
+        if( idx_dist2 == -1 ) {
+            int n = N;
+            while( s.at( n / 2 ) == s.at( 0 ) ) {
+                n /= 2;
+            }
+            if( n != 0 ) {
+                s.at( n ) = s.at( 0 );
+            } else {
+                for( int i = 1; i < N; i++ ) {
+                    if( s.at( i ) != s.at( 0 ) ) {
+                        s.at( i ) = s.at( 0 );
+                    }
+                }
+            }
+        } else {
+            for( int j = 0; j < N; j++ ) {
+                if( s.at( j ) != dict.at( idx_dist2 ).at( j ) ) {
+                    s.at( j ) = dict.at( idx_dist2 ).at( j );
+                    break;
+                }
+            }
+        }
+        t = s;
+        for( int i = 0; i < N; i++ ) {
+            dict.at( i ) = t;
+            t = t.substr( 1, N - 1 ) + t.substr( 0, 1 );
+        }
+        sum_dist = 0;
+        for( int i = 1; i < N; i++ ) {
+            dist.at( i ) = hamming_dist( dict.at( 0 ), dict.at( i ) );
+            sum_dist += dist.at( i );
+        }
+    }
+    if( sum_dist == 0 ) {
+        answer = 1;
+    }
+    else {
+        std::set< std::string > counter;
+        for( int i = 0; i < N; i ++ ) {
+            counter.insert( dict.at(i) );
+        }
+        answer = counter.size();
+    }
     std::cout << answer << std::endl;
 
     //  Finalize
