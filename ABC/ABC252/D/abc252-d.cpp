6d25ad2d6eaf94a5430641ca636230c37d75875f
keitaronaruse <naruse@u-aizu.ac.jp>
2022-05-23 10:19:16 +0900

    updated

--- ABC/ABC252/D/abc252-d.cpp
+++ ABC/ABC252/D/abc252-d.cpp
@@ -1,8 +1,8 @@
 /**
  * @file abc252-d.cpp
- * @brief ABC252 Problem D
+ * @brief ABC252 Problem D - Distinct Trio
  * @author Keitaro Naruse
- * @date 2022-05-21
+ * @date 2022-05-21, 2022-05-22
  * @copyright MIT License
  * @details https://atcoder.jp/contests/abc252/tasks/abc252_d
  */
@@ -10,15 +10,8 @@
 // # Solution
 
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
-
-template < class K, class V >
-std::ostream& operator<<( std::ostream& os, const std::pair< K, V >& p ) {
-    os << "( " << p.first << ", " << p.second << " )";
-    return ( os );
-}
 
 template < class T >
 std::ostream& operator<<( std::ostream& os, const std::vector< T >& v ) {
@@ -28,31 +21,33 @@ std::ostream& operator<<( std::ostream& os, const std::vector< T >& v ) {
     return ( os );
 }
 
-template < class T >
-std::ostream& operator<<( std::ostream& os,
-                          const std::vector< std::vector< T > >& vv ) {
-    for( const auto& v : vv ) {
-        os << v << std::endl;
-    }
-    return ( os );
-}
-
 int main( ) {
-    //  Read N = [ 1, 10^3 ]
+    //  Constant
+    const int M = 200000;
+
+    //  Read N = [ 3, 2*10^5 ]
     int N;
     std::cin >> N;
 
-    //  Read Ai = [ 0, 10^9 ]
+    //  Read Ai = [ 1, M ]
     std::vector< int > A( N );
     for( int i = 0; i < N; i++ ) {
         std::cin >> A.at( i );
     }
 
-    //  Read | S | = [ 1, 10^6 ]
-    std::string S;
-    std::cin >> S;
-
     //  Main
+    //  Preprocess
+    std::vector< int > count( M + 1, 0 );
+    for( int i = 0; i < N; i ++ ) {
+        count.at( A.at( i ) ) ++;
+    }
+    
+    std::vector< int > count_smaller( M + 1, 0 );
+    count_smaller.at( 0 ) = count.at( 0 );
+    for( int j = 1; j <= M; j ++ ) {
+        count_smaller.at( j ) = count_smaller.at( j - 1 ) + count.at( j );
+    }
+
     int answer = 0;
     std::cout << answer << std::endl;
 
