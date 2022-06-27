84b39b84e525620f14c30ae94e7917b8cdde7340
keitaronaruse <naruse@u-aizu.ac.jp>
2022-06-27 15:11:02 +0900

    updated

--- ARC/ARC143/A/arc143-a.cpp
+++ ARC/ARC143/A/arc143-a.cpp
@@ -2,7 +2,7 @@
  * @file arc143-a.cpp
  * @brief ARC143 Problem A
  * @author Keitaro Naruse
- * @date 2022-06-26
+ * @date 2022-06-26, 2022-06-27
  * @copyright MIT License
  * @details https://atcoder.jp/contests/arc143/tasks/arc143_a
  */
@@ -23,20 +23,9 @@ int main( ) {
     //  a <= b <= c
     std::vector< long long > abc = { A, B, C };
     std::sort( abc.begin( ), abc.end( ) );
-    long long k = abc.at( 0 );
-    abc.at( 0 ) -= k;
-    abc.at( 1 ) -= k;
-    abc.at( 2 ) -= k;
-    if( abc.at( 1 ) == abc.at( 2 ) ) {
-        answer = k + abc.at( 2 );
-    } else if( abc.at( 1 ) + 1 == abc.at( 2 ) ) {
-        if( k > 0L ) {
-            answer = k + abc.at( 2 );
-        } else {
-            answer = -1L;
-        }
-    } else {
-        answer = -1L;
+
+    if( abc.at( 0 ) + abc.at( 1 ) >= abc.at( 2 ) ) {
+        answer = abc.at( 2 );
     }
     std::cout << answer << std::endl;
 
