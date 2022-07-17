ee1588c163a81c6974fdc3d397acb2cb2cdc3582
keitaronaruse <naruse@u-aizu.ac.jp>
2022-07-17 20:59:25 +0900

    updated

--- ABC/ABC260/A/abc260-a.cpp
+++ ABC/ABC260/A/abc260-a.cpp
@@ -11,42 +11,11 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
-
-template < class K, class V >
-std::ostream& operator<<( std::ostream& os, const std::pair< K, V >& p ) {
-    os << "( " << p.first << ", " << p.second << " )";
-    return ( os );
-}
-
-template < class T >
-std::ostream& operator<<( std::ostream& os, const std::vector< T >& v ) {
-    for( const auto& k : v ) {
-        os << k << " ";
-    }
-    return ( os );
-}
-
-template < class T >
-std::ostream& operator<<( std::ostream& os,
-                          const std::vector< std::vector< T > >& vv ) {
-    for( const auto& v : vv ) {
-        os << v << std::endl;
-    }
-    return ( os );
-}
 
 int main( ) {
     //  Read N = [ 1, 10^3 ]
     int N;
     std::cin >> N;
-
-    //  Read Ai = [ 0, 10^9 ]
-    std::vector< int > A( N );
-    for( int i = 0; i < N; i++ ) {
-        std::cin >> A.at( i );
-    }
 
     //  Read | S | = [ 1, 10^6 ]
     std::string S;
