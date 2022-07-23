32b6fe573d0ba3555c6aa313ae70d3bb0f67f78b
keitaronaruse <naruse@u-aizu.ac.jp>
2022-07-23 17:15:00 +0900

    updated

--- ABC/ABC261/B/abc261-b.cpp
+++ ABC/ABC261/B/abc261-b.cpp
@@ -1,10 +1,10 @@
 /**
- * @file abc260-x.cpp
- * @brief ABC260 Problem X
+ * @file abc261-a.cpp
+ * @brief ABC261 Problem B
  * @author Keitaro Naruse
- * @date 2022-07-17
+ * @date 2022-07-23
  * @copyright MIT License
- * @details https://atcoder.jp/contests/abc260/tasks/abc260_x
+ * @details https://atcoder.jp/contests/abc261/tasks/abc261_b
  */
 
 // # Solution
@@ -12,27 +12,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-
-template < class K, class V >
-std::ostream& operator<<( std::ostream& os, const std::pair< K, V >& p ) {
-    os << "( " << p.first << ", " << p.second << " )";
-    return ( os );
-}
 
 template < class T >
 std::ostream& operator<<( std::ostream& os, const std::vector< T >& v ) {
     for( const auto& k : v ) {
         os << k << " ";
-    }
-    return ( os );
-}
-
-template < class T >
-std::ostream& operator<<( std::ostream& os,
-                          const std::vector< std::vector< T > >& vv ) {
-    for( const auto& v : vv ) {
-        os << v << std::endl;
     }
     return ( os );
 }
